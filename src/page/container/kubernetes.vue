405cfb93b466577f43eae5ecccfb3a14dd5291d2
caoyingjunz <cao.yingjunz@gmail.com>
2022-09-18 21:00:52 +0800

    Revert "debug"
    This reverts commit 295a9f2cfd1a15ee1f06b28e991ffde70f49e6b4.

---
+++ src/page/container/kubernetes.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>kubernetes</div>
+</template>
+
+<script>
+  export default {
+      data() {
+          return {}
+      },
+      methods: {},
+      components: {}
+  }
+</script>
