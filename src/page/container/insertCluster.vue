7d6e85d2b0f325ff5986cd65424178165e2e4452
caoyingjunz <cao.yingjunz@gmail.com>
2022-09-29 23:09:05 +0800

    新增导入标准集群页面

--- src/page/container/insertCluster.vue
+++ src/page/container/insertCluster.vue
@@ -1,3 +1,48 @@
- <template>
-    导入集群
- </template>
+<template>
+  <el-main>
+    <el-card style="margin-top: -20px; margin-left: -20px; margin-right: -20px">
+      <div class="app-pixiu-header-title">导入标准集群</div>
+    </el-card>
+    <div>dd</div>
+  </el-main>
+</template>
+
+<script setup>
+import { reactive, getCurrentInstance } from "vue";
+import { ElMessage, ElMessageBox } from "element-plus";
+import PixiuRadioCard from "@/components/radioCard/index.vue";
+const { proxy } = getCurrentInstance();
+const data = reactive({
+  loading: false,
+});
+
+const jumpRoute = (row) => {
+  proxy.$router.push({
+    name: "Kubernetes",
+    params: {
+      id: row.id,
+      name: row.name,
+    },
+  });
+};
+</script>
+
+<style>
+.box-card {
+  margin-top: 20px;
+}
+.el-main {
+  background-color: #f3f4f7;
+}
+
+.app-pixiu-header-title {
+  display: inline-block;
+  height: 30px;
+  line-height: 30px;
+  vertical-align: middle;
+  font-size: 16px;
+  font-weight: 700;
+  margin-right: 20px;
+  max-width: 70%;
+}
+</style>
