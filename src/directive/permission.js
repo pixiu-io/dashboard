c41b2497dc9e2d722265009ca184265aa945e4d4
chenqiang <cqiang1993@163.com>
2022-09-29 12:13:58 +0800

    优化dialog的使用

--- src/directive/permission.js
+++ src/directive/permission.js
@@ -3,7 +3,6 @@ import { pinia, usePermissions } from "@/store";
 const permissionsStore = usePermissions(pinia);
 
 const permissionDirective = (el, binding) => {
-  console.log(el);
   if (permissionsStore.permissions.indexOf(binding.value) < 0) {
     el.parentNode.removeChild(el);
   }
