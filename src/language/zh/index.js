749d8ce38f1a848e9efdf5d93013b67648351583
陈强 <cqiang1993@163.com>
2022-09-30 13:51:55 +0800

    V0.0.10 use i18n (#33)
    * 国际化支持

---
+++ src/language/zh/index.js
@@ -0,0 +1,10 @@
+export default {
+  message: {
+    user_login: "用户登录",
+    username: "请输入用户名",
+    password: "请输入密码",
+    switch_language: "切换语言",
+    login: "登录",
+    forget: "忘记密码",
+  },
+};
