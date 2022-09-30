602ccb32a7fc13140a20c08824304d07f8cf5305
chenqiang <cqiang1993@163.com>
2022-09-30 11:58:45 +0800

    Dockerfile一个路径问题修改,新增pixiu-card组件

---
+++ src/components/card/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    :style="`
+      width: 100%;
+      height: ${props.height};
+      background: #ffffff;
+      display: flex;
+      align-items: center;
+    `"
+  >
+    <div class="app-pixiu-header-title">
+      {{ props.title }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "默认标题",
+  },
+  height: {
+    type: String,
+    default: "60px",
+  },
+});
+</script>
+
+<style scoped>
+.app-pixiu-header-title {
+  height: 30px;
+  line-height: 30px;
+  font-size: 16px;
+  font-weight: 700;
+  padding-left: 20px;
+}
+</style>
--- src/page/container/insertCluster.vue
+++ src/page/container/insertCluster.vue
@@ -1,43 +1,44 @@
 <template>
-  <el-main>
-    <el-card style="margin-top: -20px; margin-left: -20px; margin-right: -20px">
-      <div class="app-pixiu-header-title">导入标准集群</div>
-    </el-card>
+  <div style="display: flex; flex-direction: column; width: 100%; height: 100%">
+    <pixiu-card title="导入标准集群" height="50px" />
+    <el-main>
+      <div class="app-pixiu-content-card">
+        <el-card style="margin-top: 30px; width: 75%">
+          <el-form
+            :label-position="labelPosition"
+            label-width="100px"
+            :model="formLabelAlign"
+            style="max-width: 460px"
+          >
+            <el-form-item label="集群名称">
+              <el-input v-model="formLabelAlign.name" />
+            </el-form-item>
+            <el-form-item label="所在地域">
+              <el-input v-model="formLabelAlign.region" />
+            </el-form-item>
 
-    <div class="app-pixiu-content-card">
-      <el-card style="margin-top: 30px; width: 75%">
-        <el-form
-          :label-position="labelPosition"
-          label-width="100px"
-          :model="formLabelAlign"
-          style="max-width: 460px"
-        >
-          <el-form-item label="集群名称">
-            <el-input v-model="formLabelAlign.name" />
-          </el-form-item>
-          <el-form-item label="所在地域">
-            <el-input v-model="formLabelAlign.region" />
-          </el-form-item>
+            <el-form-item label="集群描述">
+              <el-input v-model="formLabelAlign.description" />
+            </el-form-item>
 
-          <el-form-item label="集群描述">
-            <el-input v-model="formLabelAlign.description" />
-          </el-form-item>
-
-          <el-form-item>
-            <el-button type="primary" @click="submitForm(ruleFormRef)"
-              >完成</el-button
-            >
-            <el-button @click="resetForm(ruleFormRef)">取消</el-button>
-          </el-form-item>
-        </el-form>
-      </el-card>
-    </div>
-  </el-main>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm(ruleFormRef)"
+                >完成</el-button
+              >
+              <el-button @click="resetForm(ruleFormRef)">取消</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </div>
+    </el-main>
+  </div>
 </template>
 
 <script setup>
 import { reactive, getCurrentInstance, ref } from "vue";
+import PixiuCard from "@/components/card/index.vue";
 const { proxy } = getCurrentInstance();
+
 const data = reactive({
   loading: false,
 });
@@ -74,16 +75,5 @@ const jumpRoute = (row) => {
 
 .el-main {
   background-color: #f3f4f7;
-}
-
-.app-pixiu-header-title {
-  display: inline-block;
-  height: 30px;
-  line-height: 30px;
-  vertical-align: middle;
-  font-size: 16px;
-  font-weight: 700;
-  margin-right: 20px;
-  max-width: 70%;
 }
 </style>
