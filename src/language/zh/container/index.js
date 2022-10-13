3a8837fd4b69e6ff5438320d5cc8a451e9165b65
chenqiang <cqiang1993@163.com>
2022-10-13 12:17:46 +0800

    预留language内容的目录结构,修改card添加back按钮

---
+++ src/language/zh/container/index.js
@@ -0,0 +1,11 @@
+import container from "./container";
+import createCluster from "./createCluster";
+import insertCluster from "./insertCluster";
+import kubernetes from "./kubernetes";
+
+export default {
+  container,
+  createCluster,
+  insertCluster,
+  kubernetes,
+};
