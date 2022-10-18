1f7345cd11f3971d2c9ef559b227d614308c28ad
陈强 <cqiang1993@163.com>
2022-10-18 18:04:06 +0800

    Update index.js (#77)

--- src/page/index.js
+++ src/page/index.js
@@ -6,7 +6,7 @@ import Node from "./container/node.vue";
 import Home from "./home/home.vue";
 import Login from "./login/login.vue";
 import Setting from "./setting/setting.vue";
-import User from "./user/user";
+import User from "./user/user.vue";
 import Role from "./user/role.vue";
 import InsertCluster from "./container/insertCluster.vue";
 import CreateCluster from "./container/createCluster.vue";
