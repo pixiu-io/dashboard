1b603c51c0df3aa979f6c8db7a1a3b27678d20b9
caoyingjunz <cao.yingjunz@gmail.com>
2022-11-03 22:44:49 +0800

    Fix the gate

--- src/page/container/terminal.vue
+++ src/page/container/terminal.vue
@@ -1,13 +1,12 @@
 <template>
   <!-- xterm虚拟终端 -->
-  <web-shell :webshellOptipons="webshellOptipons"></web-shell>
+  <web-shell :webshelloptions="webshelloptions"></web-shell>
 </template>
 
 <script setup>
-
 import WebShell from '@/components/webshell/index.vue';
 
-const webshellOptipons = {
+const webshelloptions = {
   namespace: 'kube-system',
   cloud: 'atm-b5870ad4',
   pod: 'kubepi-679c77b8bb-2sw96',
