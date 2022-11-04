9416532ec02c7cd6ab5306f5dc1608062ada926f
noovertime7 <100392073+noovertime7@users.noreply.github.com>
2022-11-04 19:04:54 +0800

    fix(menu): fix item children undefined bug (#101)

--- src/page/container/kubernetes.vue
+++ src/page/container/kubernetes.vue
@@ -49,51 +49,81 @@ const data = reactive({
     {
       id: 1,
       name: '工作负载',
+      icon: 'icon-xitongfuzai',
+      iconType: 'iconfont',
       children: [
         {
           id: 1.1,
+          name: 'Pod',
+          url: '/kubernetes/pods',
+        },
+        {
+          id: 1.2,
           name: 'Deployment',
           url: '/kubernetes/deployments',
         },
         {
-          id: 1.2,
-          name: 'Pod',
-          url: '/kubernetes/pods',
+          id: 1.3,
+          name: 'Statsfulset',
+          url: '/kubernetes/statsfulsets',
         },
       ],
     },
     {
       id: 2,
-      name: '配置中心',
+      name: '配置',
+      icon: 'icon-peizhiguanli',
+      iconType: 'iconfont',
       children: [
         {
           id: 2.1,
           name: 'ConfigMap',
-          url: '/kubernetes/config-maps',
+          url: '/kubernetes/comfig-map',
         },
       ],
     },
     {
-      id: 3,
+      id: 4,
       name: '服务发现',
+      icon: 'icon-fuwu',
+      iconType: 'iconfont',
       children: [
         {
-          id: 3.1,
+          id: 4.1,
           name: 'Service',
           url: '/kubernetes/services',
         },
       ],
     },
     {
-      id: 4,
-      name: 'Pixiu Shell',
+      id: 3,
+      name: '存储',
+      icon: 'icon-yingpan',
+      iconType: 'iconfont',
       children: [
         {
-          id: 4.1,
-          name: 'Pixiu Shell',
-          url: '/kubernetes/terminal',
+          id: 3.1,
+          name: 'PV ',
+          url: '/kubernetes/storage-pv',
+        },
+        {
+          id: 3.2,
+          name: 'PVC',
+          url: '/kubernetes/storage-pvc',
+        },
+        {
+          id: 3.3,
+          name: 'SC',
+          url: '/kubernetes/storage-sc',
         },
       ],
+    },
+    {
+      id: 5,
+      name: 'Pixiu Shell',
+      icon: 'icon-a-kuozhanicon_huaban1fuben33',
+      iconType: 'iconfont',
+      url: '/kubernetes/terminal',
     },
   ],
 });
@@ -102,10 +132,18 @@ const changeClouds = (value) => {
   const { query } = proxy.$route;
   const { path } = proxy.$route;
   data.items.map((item) => {
-    item.children.map((childrenItem) => {
-      const url = childrenItem.url.split('?')[0];
-      childrenItem.url = `${url}?cluster=${data.cloud.cluster}`;
-    });
+    // 如果父级中不存在children,改变父级url
+    if (item.children !== undefined) {
+      // 子级url拼接集群名称
+      item.children.map((childrenItem) => {
+        const url = childrenItem.url.split('?')[0];
+        childrenItem.url = `${url}?cluster=${data.cloud.cluster}`;
+      });
+    } else {
+      // 父级url拼接集群名称
+      const url = item.url.split('?')[0];
+      item.url = `${url}?cluster=${data.cloud.cluster}`;
+    }
   });
   data.path = `${path}?cluster=${value}`;
   const newQuery = JSON.parse(JSON.stringify(query));
