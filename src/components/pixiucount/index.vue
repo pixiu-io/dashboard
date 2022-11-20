92eab1e74b035c0ed46c0a2e09106c8298e2b7fc
noovertime7 <100392073+noovertime7@users.noreply.github.com>
2022-11-20 23:44:12 +0800

    style(home): add digital scrolling components (#105)

---
+++ src/components/pixiucount/index.vue
@@ -0,0 +1,34 @@
+<template>
+  {{ d.num.toFixed(0) }}
+</template>
+
+<script setup>
+import gsap from 'gsap';
+import { reactive, watch } from 'vue';
+
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const d = reactive({
+  num: 0,
+});
+
+const AnimateTovalue = () => {
+  gsap.to(d, {
+    duration: 1,
+    num: props.value,
+  });
+};
+
+AnimateTovalue();
+
+watch(
+  () => props.value,
+  () => AnimateTovalue(),
+);
+</script>
+<style scoped></style>
