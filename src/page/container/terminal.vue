ed94eaa1ce78f97f8c42eda24ab69006b454d6b3
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-16 23:41:34 +0800

    调整ws的链接参数 (#181)

--- src/page/container/terminal.vue
+++ src/page/container/terminal.vue
@@ -7,10 +7,11 @@
 import WebShell from '@/components/webshell/index.vue';
 
 const webshelloptions = {
-  namespace: 'kube-system',
-  cloud: 'atm-b5870ad4',
-  pod: 'kubepi-679c77b8bb-2sw96',
+  namespace: 'pixiu-system',
+  cluster: 'pixiu-omsukaom',
+  pod: 'helm-toolbox-0',
   container: '',
+  command: '/bin/sh',
 };
 </script>
 
