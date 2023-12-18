4f75b671539276c456c3f33456df00d5b108f4af
darianJmy <542255405@qq.com>
2023-12-18 23:27:30 +0800

    初步更新statefulset table表格

--- src/page/container/statefulset/statefulset.vue
+++ src/page/container/statefulset/statefulset.vue
@@ -1,3 +1,396 @@
 <template>
-  <div>sts</div>
+  <el-card class="title-card-container">
+    <div class="font-container">Statefulset</div>
+  </el-card>
+
+  <div style="margin-top: 25px">
+    <el-row>
+      <el-col>
+        <button class="pixiu-two-button" @click="createStatefulset">新建</button>
+        <el-input
+          v-model="data.pageInfo.query"
+          placeholder="名称搜索关键字"
+          style="width: 480px; float: right"
+          clearable
+          @clear="getStatefulsets"
+        >
+          <template #suffix>
+            <el-icon class="el-input__icon" @click="getStatefulsets">
+              <component :is="'Search'" />
+            </el-icon>
+          </template>
+        </el-input>
+
+        <el-select
+          v-model="data.namespace"
+          style="width: 200px; float: right; margin-right: 10px"
+          @change="changeNamespace"
+        >
+          <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
+        </el-select>
+        <!-- <dev class="namespace-container" style="width: 112px; float: right">命名空间</dev> -->
+      </el-col>
+    </el-row>
+    <el-card class="box-card">
+      <el-table
+        v-loading="loading"
+        :data="data.statefulsetList"
+        stripe
+        style="margin-top: 2px; width: 100%"
+        header-row-class-name="pixiu-table-header"
+        :cell-style="{
+          'font-size': '12px',
+          color: '#29292b',
+        }"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="30" />
+
+        <el-table-column prop="metadata.name" sortable label="名称" width="180">
+          <template #default="scope">
+            <el-link class="global-table-world" type="primary" @click="jumpRoute(scope.row)">
+              {{ scope.row.metadata.name }}
+            </el-link>
+          </template>
+        </el-table-column>
+
+        <!-- <el-table-column prop="metadata.creationTimestamp" label="创建时间" width="180" /> -->
+
+        <el-table-column
+          prop="spec.template.metadata.labels"
+          label="Labels"
+          width="210"
+          :formatter="formatterLabels"
+        />
+
+        <el-table-column
+          prop="spec.selector.matchLabels"
+          label="Selector"
+          width="210"
+          :formatter="formatterLabels"
+        >
+        </el-table-column>
+
+        <el-table-column
+          prop="status"
+          label="Pod状态运行/期望"
+          width="180"
+          :formatter="formatterStatus"
+        >
+        </el-table-column>
+
+        <el-table-column
+          label="镜像"
+          prop="spec.template.spec.containers"
+          width="auto"
+          :formatter="formatterImage"
+        >
+        </el-table-column>
+
+        <el-table-column fixed="right" label="操作" width="180">
+          <template #default="scope">
+            <el-button
+              size="small"
+              type="text"
+              style="margin-right: -20px; margin-left: -10px; color: #006eff"
+              @click="editStatefulset(scope.row)"
+            >
+              设置
+            </el-button>
+
+            <el-button
+              type="text"
+              size="small"
+              style="margin-right: 1px; color: #006eff"
+              @click="handleStatefulsetScaleDialog(scope.row)"
+            >
+              调整副本数
+            </el-button>
+
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                更多
+                <el-icon><arrow-down /></el-icon>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu class="dropdown-buttons">
+                  <el-dropdown-item style="color: #006eff" @click="deleteStatefulset(scope.row)">
+                    删除
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </template>
+        </el-table-column>
+
+        <template #empty>
+          <div class="table-inline-word">选择的该命名空间的列表为空，可以切换到其他命名空间</div>
+        </template>
+      </el-table>
+
+      <el-pagination
+        v-model:currentPage="data.pageInfo.page"
+        v-model:page-size="data.pageInfo.page_size"
+        style="float: right; margin-right: 30px; margin-top: 20px; margin-bottom: 20px"
+        :page-sizes="[10, 20, 50]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="data.pageInfo.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </el-card>
+  </div>
+
+  <el-dialog
+    :model-value="data.StatefulsetReplicasDialog"
+    style="color: #000000; font: 14px"
+    width="500px"
+    center
+    @close="closeDeploymentScaleDialog"
+  >
+    <template #header>
+      <div style="text-align: left; font-weight: bold; padding-left: 5px">调整副本配置</div>
+    </template>
+
+    <el-form label-width="100px" style="max-width: 300px">
+      <el-form-item label="原副本数">
+        <el-input v-model="data.deploymentRepcliasFrom.origin" disabled />
+      </el-form-item>
+      <el-form-item label="新副本数">
+        <el-input v-model="data.deploymentRepcliasFrom.target" placeholder="请输入新副本数" />
+      </el-form-item>
+    </el-form>
+
+    <div style="margin-top: -18px"></div>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button class="pixiu-small-cancel-button" @click="closeStatefulsetScaleDialog"
+          >取消</el-button
+        >
+        <el-button
+          type="primary"
+          class="pixiu-small-confirm-button"
+          @click="confirmStatefulsetScale"
+          >确认</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
 </template>
+
+<script setup lang="jsx">
+import { useRouter } from 'vue-router';
+import { reactive, getCurrentInstance, onMounted } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import PixiuTag from '@/components/pixiuTag/index.vue';
+
+const { proxy } = getCurrentInstance();
+const router = useRouter();
+
+const data = reactive({
+  cluster: '',
+  pageInfo: {
+    page: 1,
+    limit: 10,
+    query: '',
+    total: 0,
+  },
+  loading: false,
+
+  namespace: 'default',
+  namespaces: [],
+  statefulsetList: [],
+
+  statefulsetReplicasDialog: false,
+  statefulsetRepcliasFrom: {
+    name: '',
+    origin: '',
+    target: 0,
+  },
+});
+
+const handleSizeChange = (newSize) => {
+  data.pageInfo.limit = newSize;
+  getDeployments();
+};
+
+const handleCurrentChange = (newPage) => {
+  data.pageInfo.page = newPage;
+  getDeployments();
+};
+
+// const createStatefulset = () => {
+//   const url = `/kubernetes/statefulset_create?cluster=${data.cluster}&namespace=${data.namespace}`;
+//   router.push(url);
+// };
+
+onMounted(() => {
+  data.cluster = proxy.$route.query.cluster;
+
+  getStatefulsets();
+  getNamespaceList();
+});
+
+const jumpRoute = (row) => {
+  router.push({
+    name: 'StatefulsetDetail',
+    query: {
+      cluster: data.cluster,
+      namespace: data.namespace,
+      name: row.metadata.name,
+    },
+  });
+};
+
+const getStatefulsets = async () => {
+  data.loading = true;
+  const res = await proxy.$http({
+    method: 'get',
+    url: `/proxy/pixiu/${data.cluster}/apis/apps/v1/namespaces/${data.namespace}/statefulsets`,
+    data: data.pageInfo,
+  });
+
+  console.log(res.items);
+  data.loading = false;
+  data.statefulsetList = res.items;
+  data.pageInfo.total = data.deploymentList.length;
+};
+
+const changeNamespace = async (val) => {
+  localStorage.setItem('namespace', val);
+  data.namespace = val;
+
+  getStatefulsets();
+};
+
+const getNamespaceList = async () => {
+  try {
+    const result = await proxy.$http({
+      method: 'get',
+      url: `/proxy/pixiu/${data.cluster}/api/v1/namespaces`,
+    });
+
+    for (let item of result.items) {
+      data.namespaces.push(item.metadata.name);
+    }
+  } catch (error) {}
+};
+
+// const deleteDeployment = (row) => {
+//   ElMessageBox.confirm(
+//     '此操作将永久删除 Deployment ' + row.metadata.name + ' . 是否继续?',
+//     '提示',
+//     {
+//       confirmButtonText: '确定',
+//       cancelButtonText: '取消',
+//       type: 'warning',
+//       draggable: true,
+//     },
+//   )
+//     .then(() => {
+//       const res = proxy.$http({
+//         method: 'delete',
+//         url: `/proxy/pixiu/${data.cluster}/apis/apps/v1/namespaces/${data.namespace}/deployments/${row.metadata.name}`,
+//       });
+//       ElMessage({
+//         type: 'success',
+//         message: '删除 ' + row.metadata.name + ' 成功',
+//       });
+
+//       // TODO：一次更新即可
+//       getDeployments();
+//       getDeployments();
+//     })
+//     .catch(() => {}); // 取消
+// };
+
+// const handleDeploymentScaleDialog = (row) => {
+//   data.deploymentRepcliasFrom.name = row.metadata.name;
+//   data.deploymentRepcliasFrom.target = '';
+//   data.deploymentRepcliasFrom.origin = row.spec.replicas;
+//   data.deploymentReplicasDialog = true;
+// };
+
+// const closeDeploymentScaleDialog = (row) => {
+//   data.deploymentReplicasDialog = false;
+
+//   data.deploymentRepcliasFrom.name = '';
+//   data.deploymentRepcliasFrom.origin = '';
+//   data.deploymentRepcliasFrom.target = 0;
+// };
+
+// const confirmDeploymentScale = () => {
+//   try {
+//     const res = proxy.$http({
+//       method: 'patch',
+//       url: `/proxy/pixiu/${data.cluster}/apis/apps/v1/namespaces/${data.namespace}/deployments/${data.deploymentRepcliasFrom.name}/scale`,
+//       data: {
+//         spec: {
+//           replicas: Number(data.deploymentRepcliasFrom.target),
+//         },
+//       },
+//       config: {
+//         header: {
+//           'Content-Type': 'application/merge-patch+json',
+//         },
+//       },
+//     });
+//     getStatefulset();
+//     getDeployments();
+//     closeDeploymentScaleDialog();
+//   } catch (error) {}
+// };
+
+const formatterLabels = (row, column, cellValue) => {
+  const labels = Object.entries(cellValue).map(([key, value]) => {
+    return `${key}: ${value}`;
+  });
+
+  return (
+    <div>
+      {' '}
+      {labels.map((label) => (
+        <div class="pixiu-table-formatter">{label}</div>
+      ))}{' '}
+    </div>
+  );
+};
+
+const formatterStatus = (row, column, cellValue) => {
+  return (
+    <div>
+      {cellValue.availableReplicas}/{cellValue.replicas}
+    </div>
+  );
+};
+
+const formatterImage = (row, column, cellValue) => {
+  return (
+    <div>
+      {cellValue.map((item) => (
+        <div>{item.image}</div>
+      ))}
+    </div>
+  );
+};
+</script>
+
+<style scoped="scoped">
+.font-container {
+  margin-top: -5px;
+  font-weight: bold;
+  font-size: 16px;
+  vertical-align: middle;
+}
+
+.namespace-container {
+  font-size: 14px;
+  margin-top: -2px;
+  /* margin-left: 10px; */
+  margin-right: -60px;
+  color: #4c4e58;
+  height: 20px;
+  padding: 10px;
+}
+</style>
