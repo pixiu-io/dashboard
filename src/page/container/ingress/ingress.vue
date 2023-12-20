c22dd46a3ed2d35ad8c13674992148ec04e7bf4b
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-20 17:48:28 +0800

    新增 ingress 页面 (#191)

--- src/page/container/ingress/ingress.vue
+++ src/page/container/ingress/ingress.vue
@@ -1,3 +1,209 @@
 <template>
-  <div>ingress</div>
+  <el-card class="title-card-container">
+    <div class="font-container">Ingress</div>
+  </el-card>
+
+  <div style="margin-top: 25px">
+    <el-row>
+      <el-col>
+        <button class="pixiu-two-button" @click="createService">新建</button>
+        <el-input
+          v-model="data.pageInfo.query"
+          placeholder="名称搜索关键字"
+          style="width: 480px; float: right"
+          clearable
+          @clear="getIngresses"
+        >
+          <template #suffix>
+            <el-icon class="el-input__icon" @click="getIngresses">
+              <component :is="'Search'" />
+            </el-icon>
+          </template>
+        </el-input>
+
+        <el-select
+          v-model="data.namespace"
+          style="width: 200px; float: right; margin-right: 10px"
+          @change="changeNamespace"
+        >
+          <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-card class="box-card">
+      <el-table
+        v-loading="loading"
+        :data="data.serviceList"
+        stripe
+        style="margin-top: 2px; width: 100%"
+        :cell-style="{
+          'font-size': '12px',
+          color: '#29292b',
+        }"
+        header-row-class-name="pixiu-table-header"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="30" />
+        <el-table-column prop="metadata.name" sortable label="名称" width="180">
+          <template #default="scope">
+            <el-link class="global-table-world" type="primary" @click="jumpRoute(scope.row)">
+              {{ scope.row.metadata.name }}
+            </el-link>
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="spec.type" label="HOSTS" width="110"> </el-table-column>
+        <el-table-column prop="spec.ports" label="端口组" :formatter="formatterPorts">
+        </el-table-column>
+        <el-table-column
+          label="创建时间"
+          prop="metadata.creationTimestamp"
+          width="170px"
+          :formatter="formatterTime"
+        >
+        </el-table-column>
+
+        <el-table-column fixed="right" label="操作" width="180">
+          <template #default="scope">
+            <el-button
+              size="small"
+              type="text"
+              style="margin-right: -20px; margin-left: -10px; color: #006eff"
+              @click="editIngress(scope.row)"
+            >
+              更新配置
+            </el-button>
+
+            <el-button
+              type="text"
+              size="small"
+              style="margin-right: 1px; color: #006eff"
+              @click="deleteIngress(scope.row)"
+            >
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+
+        <template #empty>
+          <div class="table-inline-word">选择的该命名空间的列表为空，可以切换到其他命名空间</div>
+        </template>
+      </el-table>
+
+      <el-pagination
+        v-model:currentPage="data.pageInfo.page"
+        v-model:page-size="data.pageInfo.page_size"
+        style="float: right; margin-right: 30px; margin-top: 20px; margin-bottom: 20px"
+        :page-sizes="[10, 20, 50]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="data.pageInfo.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </el-card>
+  </div>
 </template>
+
+<script setup lang="jsx">
+import { useRouter } from 'vue-router';
+import { formatTimestamp } from '@/utils/utils';
+import { reactive, getCurrentInstance, onMounted } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { getNamespaces } from '@/services/cloudService';
+
+const { proxy } = getCurrentInstance();
+const router = useRouter();
+
+const data = reactive({
+  cluster: '',
+  pageInfo: {
+    page: 1,
+    query: '',
+    total: 0,
+    limit: 100,
+  },
+  loading: false,
+
+  namespace: 'default',
+  namespaces: [],
+  serviceList: [],
+});
+
+const handleSizeChange = (newSize) => {
+  data.pageInfo.limit = newSize;
+  getIngresses();
+};
+
+const handleCurrentChange = (newPage) => {
+  data.pageInfo.page = newPage;
+  getIngresses();
+};
+
+onMounted(() => {
+  data.cluster = proxy.$route.query.cluster;
+
+  getIngresses();
+  getNamespaceList();
+});
+
+const getIngresses = async () => {
+  data.loading = true;
+  const res = await proxy.$http({
+    method: 'get',
+    url: `/proxy/pixiu/${data.cluster}/apis/networking.k8s.io/v1/namespaces/${data.namespace}/ingresses`,
+    data: data.pageInfo,
+  });
+
+  data.loading = false;
+  data.serviceList = res.items;
+  data.pageInfo.total = data.serviceList.length;
+};
+
+const changeNamespace = async (val) => {
+  localStorage.setItem('namespace', val);
+  data.namespace = val;
+
+  getIngresses();
+};
+
+const getNamespaceList = async () => {
+  const [err, result] = await getNamespaces(data.cluster);
+  if (err) {
+    return;
+  }
+
+  for (let item of result.items) {
+    data.namespaces.push(item.metadata.name);
+  }
+};
+
+const deleteIngress = (row) => {
+  ElMessageBox.confirm('此操作将永久删除 Ingress ' + row.metadata.name + ' . 是否继续?', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+    draggable: true,
+  })
+    .then(() => {
+      const res = proxy.$http({
+        method: 'delete',
+        url: `/proxy/pixiu/${data.cluster}/apis/networking.k8s.io/v1/namespaces/${data.namespace}/ingresses/${row.metadata.name}`,
+      });
+
+      getIngresses();
+
+      ElMessage({
+        type: 'success',
+        message: '删除 ' + row.metadata.name + ' 成功',
+      });
+    })
+    .catch(() => {}); // 取消
+};
+
+const formatterTime = (row, column, cellValue) => {
+  const time = formatTimestamp(cellValue);
+  return <div>{time}</div>;
+};
+</script>
+
+<style scoped="scoped"></style>
