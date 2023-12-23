1f3998238e81c88ff20eaa68075c4ff8daa0f827
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-23 12:38:49 +0800

    add

--- src/page/container/namespace/namespace.vue
+++ src/page/container/namespace/namespace.vue
@@ -36,20 +36,32 @@
         @selection-change="handleSelectionChange"
       >
         <!-- <el-table-column type="selection" width="30" /> -->
-        <el-table-column prop="metadata.name" sortable label="名称">
+        <el-table-column prop="metadata.name" sortable label="名称" min-width="80px">
           <template #default="scope">
             <el-link class="global-table-world" type="primary" @click="jumpRoute(scope.row)">
               {{ scope.row.metadata.name }}
             </el-link>
+
+            <el-tooltip content="复制">
+              <pixiu-icon
+                name="DocumentCopy"
+                size="10px"
+                type="el"
+                class-name="icon-box"
+                color="#909399"
+                @click="copy(scope.row)"
+              />
+            </el-tooltip>
           </template>
         </el-table-column>
 
-        <el-table-column label="状态" prop="status" :formatter="formatStatus"> </el-table-column>
+        <el-table-column label="状态" prop="status" width="160px" :formatter="formatStatus">
+        </el-table-column>
 
         <el-table-column
           label="创建时间"
           prop="metadata.creationTimestamp"
-          width="160px"
+          width="220px"
           :formatter="formatterTime"
         >
         </el-table-column>
@@ -102,6 +114,8 @@ import { useRouter } from 'vue-router';
 import { formatTimestamp } from '@/utils/utils';
 import { reactive, getCurrentInstance, onMounted } from 'vue';
 import { getNamespaces } from '@/services/cloudService';
+import useClipboard from 'vue-clipboard3';
+import { ElMessage } from 'element-plus';
 
 const { proxy } = getCurrentInstance();
 const router = useRouter();
@@ -119,6 +133,22 @@ const data = reactive({
 
   namespaceList: [],
 });
+
+const { toClipboard } = useClipboard();
+const copy = async (val) => {
+  try {
+    await toClipboard(val.name);
+    ElMessage({
+      type: 'success',
+      message: '已复制',
+    });
+  } catch (e) {
+    ElMessage({
+      type: 'error',
+      message: e.valueOf().toString(),
+    });
+  }
+};
 
 const handleSizeChange = (newSize) => {
   data.pageInfo.limit = newSize;
@@ -160,4 +190,9 @@ const formatStatus = (row, column, cellValue) => {
 };
 </script>
 
-<style scoped="scoped"></style>
+<style scoped="scoped">
+.icon-box {
+  padding: 3px;
+  margin-top: -1px;
+}
+</style>
