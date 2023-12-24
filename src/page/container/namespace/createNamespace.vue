ac6f82cd3bad30ae0848696833172d0fd23b93d7
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-24 22:39:47 +0800

    add

--- src/page/container/namespace/createNamespace.vue
+++ src/page/container/namespace/createNamespace.vue
@@ -1,1 +1,97 @@
-<template>create ns</template>
+<template>
+  <el-card class="title-card-container">
+    <div class="font-container">新建命名空间</div>
+  </el-card>
+
+  <div
+    class="deployee-class"
+    style="display: flex; flex-direction: column; width: 100%; height: 100%"
+  >
+    <el-main>
+      <div class="app-pixiu-content-card">
+        <el-card style="margin-top: 8px; width: 100%; border-radius: 0px">
+          <el-form label-position="left" label-width="100px" style="margin-left: 3%; width: 80%">
+            <div style="margin-top: 20px" />
+            <el-form-item label="名称" style="width: 35%">
+              <el-input v-model="data.namespaceForm.metadata.name" />
+              <div class="app-pixiu-line-describe2">
+                最长63个字符，只能包含小写字母、数字及分隔符("-")
+              </div>
+            </el-form-item>
+
+            <div style="margin-top: 20px" />
+            <el-form-item label="描述" style="width: 50%">
+              <el-input
+                v-model="data.namespaceForm.description"
+                placeholder="请输入命名空间的描述信息"
+                type="textarea"
+                :autosize="data.autosize"
+              />
+            </el-form-item>
+
+            <div style="margin-top: 30px" />
+            <el-form-item style="margin-left: 30%">
+              <el-button class="pixiu-cancel-button" @click="cancel()">取消</el-button>
+              <el-button class="pixiu-confirm-button" type="primary" @click="confirm()"
+                >确定</el-button
+              >
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </div>
+    </el-main>
+  </div>
+</template>
+
+<script setup>
+import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
+import PixiuCard from '@/components/card/index.vue';
+
+const { proxy } = getCurrentInstance();
+
+const data = reactive({
+  loading: false,
+  cluser: '',
+  autosize: {
+    minRows: 5,
+  },
+
+  namespaceForm: {
+    metadata: {
+      name: '',
+    },
+  },
+});
+
+const confirm = async () => {
+  try {
+    const resp = await proxy.$http({
+      method: 'post',
+      url: `/proxy/pixiu/${data.cluster}/api/v1/namespaces`,
+      data: data.namespaceForm,
+    });
+  } catch (error) {}
+
+  proxy.$message.success(`命名空间 ${data.namespaceForm.metadata.name} 创建成功`);
+  backToNamespace();
+};
+
+const cancel = () => {
+  backToNamespace();
+};
+
+onMounted(() => {
+  data.query = proxy.$route.query;
+  data.cluster = data.query.cluster;
+  data.path = proxy.$route.fullPath;
+});
+
+const backToNamespace = () => {
+  proxy.$router.push({
+    name: 'Namespace',
+    query: data.cluster,
+  });
+};
+</script>
+
+<style></style>
