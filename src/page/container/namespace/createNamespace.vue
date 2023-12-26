a6a67d3e8df99b69e54f5f675aa06e920b0b96ff
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-26 23:31:51 +0800

    添加输入框的条件限制 (#208)

--- src/page/container/namespace/createNamespace.vue
+++ src/page/container/namespace/createNamespace.vue
@@ -21,14 +21,18 @@
       <div class="app-pixiu-content-card">
         <el-card style="margin-top: 8px; width: 100%; border-radius: 0px">
           <el-form
+            ref="ruleFormRef"
             label-position="left"
             label-width="100px"
+            status-icon
             :rules="rules"
+            :model="data.namespaceForm"
+            require-asterisk-position="right"
             style="margin-left: 3%; width: 80%"
           >
             <div style="margin-top: 20px" />
 
-            <el-form-item label="名称" prop="name" style="width: 400px">
+            <el-form-item label="名称" prop="metadata.name" style="width: 400px">
               <el-input v-model="data.namespaceForm.metadata.name" />
               <div class="app-pixiu-line-describe2">
                 最长63个字符，只能包含小写字母、数字及分隔符("-")
@@ -36,7 +40,7 @@
             </el-form-item>
 
             <div style="margin-top: 20px" />
-            <el-form-item label="描述" style="width: 60%">
+            <el-form-item prop="description" label="描述" style="width: 60%">
               <el-input
                 v-model="data.namespaceForm.description"
                 placeholder="请输入命名空间的描述信息"
@@ -64,6 +68,7 @@ import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
 import PixiuCard from '@/components/card/index.vue';
 
 const { proxy } = getCurrentInstance();
+const ruleFormRef = ref();
 
 const data = reactive({
   loading: false,
@@ -79,17 +84,20 @@ const data = reactive({
   },
 });
 
-const confirm = async () => {
-  try {
-    const resp = await proxy.$http({
-      method: 'post',
-      url: `/proxy/pixiu/${data.cluster}/api/v1/namespaces`,
-      data: data.namespaceForm,
-    });
-  } catch (error) {}
-
-  proxy.$message.success(`命名空间 ${data.namespaceForm.metadata.name} 创建成功`);
-  backToNamespace();
+const confirm = () => {
+  ruleFormRef.value.validate(async (valid) => {
+    if (valid) {
+      try {
+        const resp = await proxy.$http({
+          method: 'post',
+          url: `/proxy/pixiu/${data.cluster}/api/v1/namespaces`,
+          data: data.namespaceForm,
+        });
+        proxy.$message.success(`命名空间 ${data.namespaceForm.metadata.name} 创建成功`);
+        backToNamespace();
+      } catch (error) {}
+    }
+  });
 };
 
 const cancel = () => {
@@ -104,7 +112,7 @@ onMounted(() => {
 });
 
 const rules = {
-  name: [{ required: true, message: '请输入命名空间名称', trigger: 'blur' }],
+  'metadata.name': [{ required: true, message: '请输入命名空间名称', trigger: 'blur' }],
 };
 
 const backToNamespace = () => {
