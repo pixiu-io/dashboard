a5f9cf35a508297e528194ebbdfd72fc8e9341a8
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-27 08:53:49 +0800

    add

--- src/page/container/configmap/createConfigMap.vue
+++ src/page/container/configmap/createConfigMap.vue
@@ -24,19 +24,21 @@
             label-position="left"
             require-asterisk-position="right"
             label-width="100px"
+            ref="ruleFormRef"
+            :rules="rules"
+            status-icon
+            :model="data.configmapForm"
             style="margin-left: 3%; width: 80%"
           >
             <div style="margin-top: 20px" />
-            <el-form-item
-              :rules="[{ required: true, message: '名称不能为空', trigger: 'blur' }]"
-              label="名称"
-              style="width: 700px"
-            >
-              <el-input v-model="data.configmapForm.metadata.name" style="width: 200px" />
+
+            <el-form-item label="名称" prop="metadata.name" style="width: 80%">
+              <el-input style="width: 40%" v-model="data.configmapForm.metadata.name" />
               <div class="app-pixiu-line-describe2">
                 最长63个字符，只能包含小写字母、数字及分隔符("-"),且必须以小写字母开头，数字或小写字母结尾
               </div>
             </el-form-item>
+
             <el-form-item label="命名空间" style="width: 300px">
               <div class="namespace-select-container">
                 <el-select
@@ -117,6 +119,7 @@
 import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
 import PixiuCard from '@/components/card/index.vue';
 const { proxy } = getCurrentInstance();
+const ruleFormRef = ref();
 
 const data = reactive({
   loading: false,
@@ -150,22 +153,30 @@ const data = reactive({
   },
 });
 
-const comfirmCreate = async () => {
-  data.configMapLabels.forEach((item) => {
-    data.configmapForm.data[item.key] = item.value;
+const rules = {
+  'metadata.name': [{ required: true, message: '请输入 ConfigMap 名称', trigger: 'blur' }],
+};
+
+const comfirmCreate = () => {
+  ruleFormRef.value.validate(async (valid) => {
+    if (valid) {
+      data.configMapLabels.forEach((item) => {
+        data.configmapForm.data[item.key] = item.value;
+      });
+      try {
+        const resp = await proxy.$http({
+          method: 'post',
+          url:
+            `/proxy/pixiu/${data.cluster}/api/v1/namespaces/` +
+            data.configmapForm.metadata.namespace +
+            `/configmaps`,
+          data: data.configmapForm,
+        });
+        proxy.$message.success(`configmap ${data.configmapForm.metadata.name} 创建成功`);
+        backToConfigmap();
+      } catch (error) {}
+    }
   });
-  try {
-    const resp = await proxy.$http({
-      method: 'post',
-      url:
-        `/proxy/pixiu/${data.cluster}/api/v1/namespaces/` +
-        data.configmapForm.metadata.namespace +
-        `/configmaps`,
-      data: data.configmapForm,
-    });
-    proxy.$message.success(`configmap ${data.configmapForm.metadata.name} 创建成功`);
-    backToConfigmap();
-  } catch (error) {}
 };
 
 const cancelCreate = () => {
