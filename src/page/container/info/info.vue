7e3cc384985991309ad56bae980311f1ff5ae77f
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-30 00:28:06 +0800

    add

--- src/page/container/info/info.vue
+++ src/page/container/info/info.vue
@@ -7,11 +7,12 @@
     <el-card class="content1-card-container"> 集群信息</el-card>
     <el-card class="content2-card-container"> 集群资源使用情况</el-card>
   </dev>
+  <div><el-card class="content3-card-container"> 核心组件状态</el-card></div>
 </template>
 
 <style>
 .content1-card-container {
-  height: 500px;
+  height: 400px;
   width: 30%;
   margin-top: 25px;
   margin-left: 10px;
@@ -19,12 +20,19 @@
 }
 
 .content2-card-container {
-  height: 500px;
+  height: 400px;
   width: 100%;
   margin-top: 25px;
   margin-left: 25px;
   margin-right: 10px;
+  border-radius: 0px;
+}
 
+.content3-card-container {
+  height: 380px;
+  width: 98.5%;
+  margin-top: 25px;
+  margin-left: 10px;
   border-radius: 0px;
 }
 </style>
