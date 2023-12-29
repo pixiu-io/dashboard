8da19b822d35c06dd8e3587f1309f824b9f428de
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-30 01:02:43 +0800

    add

--- src/page/container/info/info.vue
+++ src/page/container/info/info.vue
@@ -10,10 +10,37 @@
   <div><el-card class="content3-card-container"> 核心组件状态</el-card></div>
 </template>
 
+<script setup>
+import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
+import PixiuCard from '@/components/card/index.vue';
+const { proxy } = getCurrentInstance();
+const ruleFormRef = ref();
+
+const data = reactive({
+  cluster: '',
+  clusterForm: {
+    name: '',
+    alias_name: '',
+    cluster_type: 0,
+    protected: true,
+    kubernetes_version: '',
+    gmt_create: '',
+  },
+});
+
+onMounted(() => {
+  data.cluster = proxy.$route.query.cluster;
+
+  getCluster();
+});
+
+const getCluster = async () => {};
+</script>
+
 <style>
 .content1-card-container {
   height: 400px;
-  width: 30%;
+  width: 38%;
   margin-top: 25px;
   margin-left: 10px;
   border-radius: 0px;
