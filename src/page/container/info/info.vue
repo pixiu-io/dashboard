5cb2335805992750251ae72457cbe737a727a9d1
caoyingjunz <cao.yingjunz@gmail.com>
2023-12-29 08:57:35 +0800

    add

--- src/page/container/info/info.vue
+++ src/page/container/info/info.vue
@@ -3,18 +3,28 @@
     <div class="font-container">基本信息</div>
   </el-card>
 
-  <dev style="margin-top: 20px"></dev>
-
-  <el-card class="content-card-container"> 集群信息</el-card>
+  <dev class="one-line-style">
+    <el-card class="content1-card-container"> 集群信息</el-card>
+    <el-card class="content2-card-container"> 集群资源使用情况</el-card>
+  </dev>
 </template>
 
 <style>
-/* 标题卡片 */
-.content-card-container {
+.content1-card-container {
   height: 500px;
-  width: 400px;
+  width: 30%;
   margin-top: 25px;
-  margin-left: 6px;
+  margin-left: 10px;
+  border-radius: 0px;
+}
+
+.content2-card-container {
+  height: 500px;
+  width: 100%;
+  margin-top: 25px;
+  margin-left: 25px;
+  margin-right: 10px;
+
   border-radius: 0px;
 }
 </style>
