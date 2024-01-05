ecb67684a175dacfe271d122237499bfb02ca657
caoyingjunz <cao.yingjunz@gmail.com>
2024-01-05 22:28:09 +0800

    Refactor the login page

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,43 +1,33 @@
 <template>
+  <div class="header"></div>
   <div class="login-page">
     <div class="login-container">
+      <div class="txt-container">Pixiu</div>
       <div class="tab-container">
         <div class="tab-item">
           <div selected="selected" class="u-tabs_item_3_DeFFee">
             {{ $t(`login.user_login`) }}
           </div>
-          <div class="change-language-container">
-            <div style="cursor: pointer" @click="i18nStore.changeLanguage">
-              <el-icon class="el-input__icon">
-                <component :is="'Switch'" />
-              </el-icon>
-              {{ $t(`login.switch_language`) }}
-            </div>
-          </div>
         </div>
         <el-form
           :ref="(el) => (loginStore.loginFormRef = el)"
           :model="loginStore.loginInfo"
-          :rules="rules"
           style="width: 100%; height: 30%"
         >
-          <el-form-item prop="name">
+          <el-form-item prop="name" class="inputDeep">
             <el-input
               v-model="loginStore.loginInfo.name"
+              class="el-input"
               :placeholder="$t(`login.username`)"
               clearable
               maxlength="128"
               size="large"
+              :border="false"
               @keyup.enter="loginStore.loginFn"
             >
-              <template #prefix>
-                <el-icon class="el-input__icon">
-                  <component :is="'UserFilled'" />
-                </el-icon>
-              </template>
             </el-input>
           </el-form-item>
-          <el-form-item prop="password">
+          <el-form-item prop="password" class="inputDeep">
             <el-input
               v-model="loginStore.loginInfo.password"
               :placeholder="$t(`login.password`)"
@@ -47,20 +37,20 @@
               size="large"
               @keyup.enter="loginStore.loginFn"
             >
-              <template #prefix>
-                <el-icon class="el-input__icon">
-                  <component :is="'Lock'"></component>
-                </el-icon>
-              </template>
             </el-input>
           </el-form-item>
         </el-form>
-        <div class="button-group">
+        <div style="display: grid; grid-template-columns: 1fr 1fr">
+          <div>
+            <el-checkbox class="checkbox" label="记住我" size="large" />
+          </div>
           <div class="forget-container">
             <el-button class="forget-button" type="text" size="large" @click="loginStore.forgetFn">
               {{ $t(`login.forget`) }}
             </el-button>
           </div>
+        </div>
+        <div class="button-group">
           <el-button
             class="login-button"
             size="large"
@@ -70,6 +60,11 @@
             {{ $t(`login.login`) }}</el-button
           >
         </div>
+        <div>
+          <el-link class="more" href="https://github.com/pixiu-io/dashboard" target="_blank">
+            更多信息...
+          </el-link>
+        </div>
       </div>
     </div>
   </div>
@@ -77,35 +72,25 @@
 
 <script setup>
 import useLoginStore from '@/stores/useLogin';
-import useI18nStore from '@/stores/useI18n';
-const rules = {
-  name: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-  password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
-};
 const loginStore = useLoginStore();
-const i18nStore = useI18nStore();
 </script>
 
 <style scoped>
 /* 有用 */
 .login-page {
   display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 100%;
 }
 
 .login-container {
-  width: 450px;
+  width: 500px;
   height: 380px;
   box-sizing: border-box;
   background: #fff;
+  margin-left: 80px;
+  margin-top: 230px;
   transition: box-shadow 0.2s;
-  box-shadow: 0 0 10px 0 rgb(80 90 109 / 16%);
-  display: flex;
-  align-items: center;
-  justify-content: center;
 }
 
 .tab-container {
@@ -124,7 +109,7 @@ const i18nStore = useI18nStore();
 
 .u-tabs_item_3_DeFFee {
   cursor: pointer;
-  display: inline-block;
+  display: none;
   box-sizing: content-box;
   height: 30px;
   line-height: 30px;
@@ -132,15 +117,6 @@ const i18nStore = useI18nStore();
   border: 1px solid #ece5e3;
   border-bottom: none;
   /* margin-bottom: -1px; */
-}
-
-.change-language-container {
-  width: 70%;
-  border-bottom: 1px solid #ece5e3;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  color: #508ae2;
 }
 
 /* 有用 */
@@ -151,40 +127,69 @@ const i18nStore = useI18nStore();
   border-top: 2px solid #508ae2;
 }
 
-.u-tabs_head_3_DeFFee {
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  height: 30px;
-  border-bottom: 1px solid #ece5e3;
-  width: 100%;
-}
-
 .button-group {
   width: 100%;
   height: 40%;
+  margin-top: 80px;
 }
 
 .forget-container {
   display: flex;
   width: 100%;
   justify-content: flex-end;
+  margin-top: 30px;
 }
 
 .forget-button {
-  font-size: inherit;
-  color: #6383dc;
+  font-size: 15px;
+  color: #007cbb;
+  margin-top: -23px;
+  margin-left: 100px;
+}
+
+.checkbox {
+  margin-top: 5px;
+  color: #007cbb;
 }
 
 .login-button {
-  width: 100%;
-  background: #508ae2;
-  color: #fff;
-  font-size: 17px;
+  width: 105%;
+  height: 35px;
+  background: #e7eaea;
+  color: #ccc;
+  font-size: 13px;
 }
 
-.el-form-item {
+.inputDeep {
+  :deep(.el-input__wrapper) {
+    box-shadow: 0 0 0 0px var(--el-input-border-color, var(--el-border-color)) inset;
+    cursor: default;
+    .el-input__inner {
+      cursor: default !important;
+    }
+    border-bottom: 1px solid grey;
+  }
+}
+
+.el-input {
+  width: 160%;
+  height: 30px;
+}
+.header {
   width: 100%;
+  height: 60px;
+  background-color: rgb(0, 74, 112);
+}
+
+.txt-container {
+  margin-top: 125px;
+  height: 100px;
+  font-size: 35px;
+}
+
+.more {
+  margin-left: 335px;
+  font-size: 13px;
+  color: #007cbb;
 }
 </style>
