ab3a7867985165299a71fdf4a3fe18d68f14f85a
caoyingjunz <cao.yingjunz@gmail.com>
2024-01-06 01:07:45 +0800

    add

--- src/page/container/deployment/details.vue
+++ src/page/container/deployment/details.vue
@@ -11,117 +11,119 @@
   </el-card>
 
   <el-card style="border-radius: 0px; margin-top: 20px; margin-left: 2px; margin-right: 2px">
-    <el-tabs
-      v-model="data.activeName"
-      class="deployment-tab"
-      @tab-click="handleClick"
-      @tab-change="handleChange"
-    >
-      <el-tab-pane label="基本信息" name="first">
-        <div class="app-pixiu-content-card">
-          <div
-            v-if="data.deployment.metadata"
-            style="margin-top: 8px; width: 100%; border-radius: 0px"
-          >
-            <el-form-item label="名称" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 90px">
-                {{ data.deployment.metadata.name }}
-              </span>
-            </el-form-item>
-            <el-form-item label="命名空间" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 63px">
-                {{ data.deployment.metadata.namespace }}
-              </span>
-            </el-form-item>
-            <el-form-item label="创建时间" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 63px">
-                {{ data.deployment.metadata.creationTimestamp }}
-              </span>
-            </el-form-item>
-            <el-form-item label="Labels" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 75px">
-                {{ data.deployment.spec.selector.matchLabels }}
-              </span>
-            </el-form-item>
-            <el-form-item label="更新策略" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 63px">
-                {{ data.deployment.spec.strategy.type }}
-              </span>
-            </el-form-item>
-            <el-form-item label="副本数" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 75px">
-                {{ data.deployment.spec.replicas }}
-              </span>
-            </el-form-item>
-            <el-form-item label="其他" class="deployment-info">
-              <span class="deploy-detail-info" style="margin-left: 88px"> - </span>
-            </el-form-item>
-          </div>
-        </div>
-      </el-tab-pane>
-
-      <el-tab-pane label="Pod管理" name="second"
-        ><div style="margin-top: 8px">
-          <el-table
-            v-loading="data.loading"
-            :data="data.deploymentPods"
-            stripe
-            style="margin-top: 2px; width: 100%"
-            header-row-class-name="pixiu-table-header"
-            @selection-change="handleSelectionChange"
-            :cell-style="{
-              'font-size': '12px',
-              color: '#29292b',
-            }"
-          >
-            <el-table-column type="selection" width="30" />
-            <el-table-column prop="metadata.name" label="实例名称" min-width="200px" />
-            <el-table-column
-              prop="metadata.creationTimestamp"
-              label="创建时间"
-              width="170px"
-              :formatter="formatterTime"
-            />
-            <el-table-column
-              prop="status"
-              label="状态"
-              :formatter="formatterStatus"
-              min-width="80px"
-            />
-            <el-table-column prop="status.hostIP" label="所在节点" width="120px" />
-            <el-table-column prop="status.podIP" label="实例IP" width="120px" />
-            <el-table-column
-              prop="status.containerStatuses"
-              label="重启次数"
-              width="80px"
-              :formatter="getPodRestartCount"
-            />
-            <el-table-column fixed="right" label="操作" width="160px">
-              <template #default="scope">
-                <el-button
-                  size="small"
-                  type="text"
-                  style="margin-right: -25px; margin-left: -10px; color: #006eff"
-                  @click="deletePod(scope.row)"
-                >
-                  销毁重建
-                </el-button>
-
-                <el-button
-                  type="text"
-                  size="small"
-                  style="margin-right: 1px; color: #006eff"
-                  @click="getPodLog(scope.row)"
-                >
-                  查看日志
-                </el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-        </div></el-tab-pane
+    <div style="margin-left: 15px">
+      <el-tabs
+        v-model="data.activeName"
+        class="deployment-tab"
+        @tab-click="handleClick"
+        @tab-change="handleChange"
       >
-      <el-tab-pane label="事件" name="third">Role</el-tab-pane>
-    </el-tabs>
+        <el-tab-pane label="基本信息" name="first">
+          <div class="app-pixiu-content-card">
+            <div
+              v-if="data.deployment.metadata"
+              style="margin-top: 8px; width: 100%; border-radius: 0px"
+            >
+              <el-form-item label="名称" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 90px">
+                  {{ data.deployment.metadata.name }}
+                </span>
+              </el-form-item>
+              <el-form-item label="命名空间" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 63px">
+                  {{ data.deployment.metadata.namespace }}
+                </span>
+              </el-form-item>
+              <el-form-item label="创建时间" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 63px">
+                  {{ data.deployment.metadata.creationTimestamp }}
+                </span>
+              </el-form-item>
+              <el-form-item label="Labels" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 75px">
+                  {{ data.deployment.spec.selector.matchLabels }}
+                </span>
+              </el-form-item>
+              <el-form-item label="更新策略" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 63px">
+                  {{ data.deployment.spec.strategy.type }}
+                </span>
+              </el-form-item>
+              <el-form-item label="副本数" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 75px">
+                  {{ data.deployment.spec.replicas }}
+                </span>
+              </el-form-item>
+              <el-form-item label="其他" class="deployment-info">
+                <span class="deploy-detail-info" style="margin-left: 88px"> - </span>
+              </el-form-item>
+            </div>
+          </div>
+        </el-tab-pane>
+
+        <el-tab-pane label="Pod管理" name="second"
+          ><div style="margin-top: 8px">
+            <el-table
+              v-loading="data.loading"
+              :data="data.deploymentPods"
+              stripe
+              style="margin-top: 2px; width: 100%"
+              header-row-class-name="pixiu-table-header"
+              @selection-change="handleSelectionChange"
+              :cell-style="{
+                'font-size': '12px',
+                color: '#29292b',
+              }"
+            >
+              <el-table-column type="selection" width="30" />
+              <el-table-column prop="metadata.name" label="实例名称" min-width="200px" />
+              <el-table-column
+                prop="metadata.creationTimestamp"
+                label="创建时间"
+                width="170px"
+                :formatter="formatterTime"
+              />
+              <el-table-column
+                prop="status"
+                label="状态"
+                :formatter="formatterStatus"
+                min-width="80px"
+              />
+              <el-table-column prop="status.hostIP" label="所在节点" width="120px" />
+              <el-table-column prop="status.podIP" label="实例IP" width="120px" />
+              <el-table-column
+                prop="status.containerStatuses"
+                label="重启次数"
+                width="80px"
+                :formatter="getPodRestartCount"
+              />
+              <el-table-column fixed="right" label="操作" width="160px">
+                <template #default="scope">
+                  <el-button
+                    size="small"
+                    type="text"
+                    style="margin-right: -25px; margin-left: -10px; color: #006eff"
+                    @click="deletePod(scope.row)"
+                  >
+                    销毁重建
+                  </el-button>
+
+                  <el-button
+                    type="text"
+                    size="small"
+                    style="margin-right: 1px; color: #006eff"
+                    @click="getPodLog(scope.row)"
+                  >
+                    查看日志
+                  </el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div></el-tab-pane
+        >
+        <el-tab-pane label="事件" name="third">Role</el-tab-pane>
+      </el-tabs>
+    </div>
   </el-card>
 
   <el-drawer v-model="data.drawer" title="I am the title" :with-header="false" size="35%">
