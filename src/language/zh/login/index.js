9d1b06f704e2027b3c68aa91054b32210a95f7b6
caoyingjunz <cao.yingjunz@gmail.com>
2024-01-14 11:59:52 +0800

    Refactor the login page [part1] (#216)

--- src/language/zh/login/index.js
+++ src/language/zh/login/index.js
@@ -1,7 +1,7 @@
 export default {
   user_login: '用户登录',
-  username: '请输入用户名',
-  password: '请输入密码',
+  username: '用户名',
+  password: '密码',
   switch_language: 'SwitchLanguage',
   login: '登录',
   forget: '忘记密码',
