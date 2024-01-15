f48acc3427d60d0a851156b4623964b8b0f36ea0
huluqaqxq <74292567+huluqaqxq@users.noreply.github.com>
2024-01-16 07:30:42 +0800

    优化更新页面 基本信息样式

--- src/page/container/secret/editSecret.vue
+++ src/page/container/secret/editSecret.vue
@@ -25,20 +25,19 @@
             :model="data.configmapForm"
             style="margin-left: 3%; width: 80%"
           >
-            <el-form-item label="基本信息" style="margin-top: 20px">
+            <el-form-item label="基本信息" style="margin-top: 20px" class="base-info">
+            </el-form-item>
+            <el-form-item>
               <el-form
                 label-position="left"
                 require-asterisk-position="right"
-                style="margin-top: 20px; margin-left: -100px"
+                style="margin-top: -20px; margin-left: -100px"
               >
-                <el-form-item
-                  class="configmap-info"
-                  label="所在地域"
-                  style="margin-top: 20px; width: 200px"
-                  ><el-form-item style="margin-left: 50px" label="华东地区(南京)"></el-form-item>
-                </el-form-item>
                 <el-form-item label="集群ID" style="margin-top: 10px; width: 200px">
-                  <el-form-item style="margin-left: 61px" :label="data.secretFrom.metadata.uid">
+                  <el-form-item
+                    style="margin-left: 61px"
+                    :label="data.cloud.name + '(' + data.cloud.name + ')'"
+                  >
                   </el-form-item>
                 </el-form-item>
                 <el-form-item label="所在命名空间" style="margin-top: 10px; width: 200px">
@@ -248,7 +247,6 @@ const cancelUpdate = () => {
 onMounted(() => {
   data.cloud = proxy.$route.query;
   data.secretFrom.metadata.name = data.cloud.name;
-  data.path = proxy.$route.fullPath;
   getSecret();
   getNamespace();
   getNamespaceList();
@@ -369,6 +367,11 @@ const deleteLabel = (index) => {
   font-weight: bold;
   font-size: 16px;
   vertical-align: middle;
+}
+
+.base-info .el-form-item__label {
+  color: black;
+  font-weight: bolder;
 }
 
 .deploy-pixiu-column {
