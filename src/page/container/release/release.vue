8f593764582ea33d8c002f37908772488d9152ee
caoyingjunz <cao.yingjunz@gmail.com>
2024-01-20 22:20:31 +0800

    add

--- src/page/container/release/release.vue
+++ src/page/container/release/release.vue
@@ -15,7 +15,7 @@
         <el-input
           v-model="data.pageInfo.query"
           placeholder="名称搜索关键字"
-          style="width: 420px; float: right"
+          style="width: 480px; float: right"
           clearable
           @input="getReleases"
           @clear="getReleases"
@@ -29,7 +29,7 @@
 
         <el-select
           v-model="data.namespace"
-          style="width: 160px; float: right; margin-right: 10px"
+          style="width: 200px; float: right; margin-right: 10px"
           @change="changeNamespace"
         >
           <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
