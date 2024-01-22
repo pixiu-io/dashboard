795d88a86c333ebfc562508a28325f6a4c0efb4d
caoyingjunz <cao.yingjunz@gmail.com>
2024-01-22 21:48:42 +0800

    Update index.vue

--- src/components/pixiuyaml/index.vue
+++ src/components/pixiuyaml/index.vue
@@ -94,7 +94,7 @@ const closeYamlDialog = () => {
 
 const checkEmpty = (name, value) => {
   if (value === undefined || value === '' || value === null) {
-    ElMessage.warning(`${name}为必填项`);
+    ElMessage.warning(`${name} 为必填项`);
     return true;
   }
   return false;
@@ -120,40 +120,6 @@ const confirmYaml = async () => {
   ) {
     return;
   }
-  // TODO: 待优化
-  // const kind = yamlData.kind;
-  // if (kind === undefined || kind === '' || kind === null) {
-  //   ElMessage({
-  //     message: 'kind 为必填项',
-  //     type: 'warning',
-  //   });
-  //   return;
-  // }
-  //
-  // const metadata = yamlData.metadata;
-  // if (metadata === undefined || metadata === '' || metadata === null) {
-  //   ElMessage({
-  //     message: 'metadata 为必填项',
-  //     type: 'warning',
-  //   });
-  //   return;
-  // }
-  // const name = metadata.name;
-  // if (name === undefined || name === '' || name === null) {
-  //   ElMessage({
-  //     message: 'metadata.name 为必填项',
-  //     type: 'warning',
-  //   });
-  //   return;
-  // }
-  // const namespace = metadata.namespace;
-  // if (namespace === undefined || namespace === '' || namespace === null) {
-  //   ElMessage({
-  //     message: 'metadata.namespace 为必填项',
-  //     type: 'warning',
-  //   });
-  //   return;
-  // }
 
   let baseUrl = `/proxy/pixiu/${data.cluster}`;
   if (kind === 'Secret') {
