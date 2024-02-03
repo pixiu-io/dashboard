c88351cf041922ace34b3ede45494f1c5723e093
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-03 23:25:00 +0800

    新增 ingress 的创建交互页面

--- src/page/container/ingress/createIngress.vue
+++ src/page/container/ingress/createIngress.vue
@@ -1,1 +1,151 @@
-<template>create ing</template>
+<template>
+  <el-card class="title-card-container">
+    <div class="font-container">创建 Ingress</div>
+  </el-card>
+
+  <el-main>
+    <el-card class="create-card-style">
+      <el-form
+        ref="ruleFormRef"
+        :rules="rules"
+        label-position="left"
+        label-width="100px"
+        require-asterisk-position="right"
+        status-icon
+        :model="data.form"
+        style="margin-left: 3%; width: 80%"
+      >
+        <div style="margin-top: 20px" />
+        <el-form-item label="名称" prop="metadata.name" style="width: 500px">
+          <el-input v-model="data.form.metadata.name" />
+          <div class="app-pixiu-line-describe2">
+            最长63个字符，只能包含小写字母、数字及分隔符("-")
+          </div>
+        </el-form-item>
+
+        <div style="margin-top: -10px" />
+        <el-form-item label="命名空间" style="width: 380px">
+          <div class="namespace-select-container">
+            <el-select v-model="data.form.metadata.namespace" @change="changeNamespace">
+              <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
+            </el-select>
+          </div>
+
+          <div style="margin-left: 10px; margin-top: 6px">
+            <pixiu-icon
+              name="icon-icon-refresh"
+              style="cursor: pointer"
+              size="16px"
+              type="iconfont"
+              color="#909399"
+              @click="getNamespaceList"
+            />
+          </div>
+        </el-form-item>
+
+        <div style="margin-top: 20px" />
+        <el-form-item label="端口" style="width: 600px">
+          <el-radio-group v-model="data.targetPort" style="margin-top: 4px">
+            <el-radio-button label="80" border>80</el-radio-button>
+            <el-radio-button label="443" border>443</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+
+        <div style="margin-top: -6px" />
+        <el-form-item label="转发规则" style="width: 600px"> </el-form-item>
+
+        <el-form-item label="会话保持" style="margin-top: 20px">
+          <el-switch v-model="data.Session" inline-prompt width="40px" />
+        </el-form-item>
+
+        <div style="margin-top: 30px" />
+        <el-form-item style="margin-left: 30%">
+          <el-button class="pixiu-cancel-button" @click="cancel()">取消</el-button>
+          <el-button class="pixiu-confirm-button" type="primary" @click="comfirm()">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </el-main>
+</template>
+
+<script setup>
+import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
+import PixiuCard from '@/components/card/index.vue';
+
+const ruleFormRef = ref();
+const { proxy } = getCurrentInstance();
+
+const data = reactive({
+  loading: false,
+  Session: false,
+
+  clutser: '',
+  namespaces: [],
+
+  targetPort: 80,
+  form: {
+    metadata: {
+      name: '',
+      namespace: 'default',
+    },
+    spec: {},
+  },
+});
+
+const rules = {
+  'metadata.name': [{ required: true, message: '请输入 Ingress 名称', trigger: 'blur' }],
+};
+
+onMounted(() => {
+  data.query = proxy.$route.query;
+  data.cluster = data.query.cluster;
+
+  data.path = proxy.$route.fullPath;
+
+  getNamespaceList();
+});
+
+watch(
+  () => data.selectorType,
+  (newActive, oldActive) => {},
+);
+
+const comfirm = async () => {
+  ruleFormRef.value.validate(async (valid) => {
+    if (valid) {
+    }
+  });
+};
+
+const cancel = () => {
+  backToIngress();
+};
+
+const changeNamespace = async (val) => {
+  localStorage.setItem('namespace', val);
+  data.form.metadata.namespace = val;
+};
+
+const getNamespaceList = async () => {
+  try {
+    const result = await proxy.$http({
+      method: 'get',
+      url: '/proxy/pixiu/' + data.cluster + '/api/v1/namespaces',
+    });
+
+    data.namespaces = [];
+    for (let item of result.items) {
+      data.namespaces.push(item.metadata.name);
+    }
+  } catch (error) {}
+};
+
+const backToIngress = () => {
+  proxy.$router.push({
+    name: 'Ingress',
+    query: data.query,
+  });
+};
+</script>
+
+<style></style>
