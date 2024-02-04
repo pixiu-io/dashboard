7d1fb9e9c969e9f05b2a25d974b143ef1632f07c
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-04 22:35:04 +0800

    add

--- src/page/container/deployment/createDeployment.vue
+++ src/page/container/deployment/createDeployment.vue
@@ -1,153 +1,131 @@
 <template>
   <el-card class="title-card-container">
-    <div class="font-container">创建 Deployment</div>
+    <div class="font-container">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item @click="backToDeployment"
+          ><span style="color: black; cursor: pointer"> Deployment </span>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item style="color: black">{{ data.cluster }}</el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <span style="color: black"> 新建 Deployment </span>
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
   </el-card>
 
-  <div
-    class="deployee-class"
-    style="display: flex; flex-direction: column; width: 100%; height: 100%"
-  >
-    <el-main>
-      <div class="app-pixiu-content-card">
-        <el-card style="margin-top: 8px; width: 100%; border-radius: 0px">
-          <el-form
-            ref="ruleFormRef"
-            label-position="left"
-            require-asterisk-position="right"
-            label-width="100px"
-            :rules="rules"
-            status-icon
-            :model="data.deploymentForm"
-            style="margin-left: 3%; width: 70%"
-          >
-            <div style="margin-top: 20px" />
-            <el-form-item label="名称" prop="metadata.name" style="width: 500px">
-              <el-input v-model="data.deploymentForm.metadata.name" />
-              <div class="app-pixiu-line-describe2">
-                最长63个字符，只能包含小写字母、数字及分隔符("-")
-              </div>
-            </el-form-item>
+  <el-card class="create-card-style">
+    <el-form
+      ref="ruleFormRef"
+      label-position="left"
+      require-asterisk-position="right"
+      label-width="100px"
+      :rules="rules"
+      status-icon
+      :model="data.deploymentForm"
+      style="margin-left: 3%; width: 70%"
+    >
+      <div style="margin-top: 20px" />
+      <el-form-item label="名称" prop="metadata.name" style="width: 500px">
+        <el-input v-model="data.deploymentForm.metadata.name" />
+        <div class="app-pixiu-line-describe2">
+          最长63个字符，只能包含小写字母、数字及分隔符("-")
+        </div>
+      </el-form-item>
 
-            <el-form-item label="命名空间" style="width: 300px">
-              <div class="namespace-select-container">
-                <el-select
-                  v-model="data.deploymentForm.metadata.namespace"
-                  @change="changeNamespace"
-                >
-                  <el-option
-                    v-for="item in data.namespaces"
-                    :key="item"
-                    :value="item"
-                    :label="item"
-                  />
-                </el-select>
-              </div>
-            </el-form-item>
+      <el-form-item label="命名空间" style="width: 300px">
+        <div class="namespace-select-container">
+          <el-select v-model="data.deploymentForm.metadata.namespace" @change="changeNamespace">
+            <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
+          </el-select>
+        </div>
+      </el-form-item>
 
-            <el-form-item label="Labels" style="margin-top: 20px">
-              <el-button type="text" class="app-action-btn" @click="addLabel">新增</el-button>
-            </el-form-item>
+      <el-form-item label="Labels" style="margin-top: 20px">
+        <el-button type="text" class="app-action-btn" @click="addLabel">新增</el-button>
+      </el-form-item>
 
-            <el-form-item
-              v-for="(item, index) in data.deploymentLabels"
-              :key="index"
-              style="margin-top: -15px"
-            >
-              <div>
-                <el-input v-model="item.key" placeholder="标签键" style="width: 200px" />
-              </div>
-              <div style="margin-right: 10px; margin-left: 10px"></div>
-              <div>
-                <el-input v-model="item.value" placeholder="标签值" style="width: 200px" />
-              </div>
-              <div
-                style="float: right; cursor: pointer; margin-left: 10px"
-                @click="deleteLabel(index)"
-              >
-                <el-icon><Delete /></el-icon>
-              </div>
-            </el-form-item>
-            <div class="app-pixiu-line-describe">
-              标签键值以字母、数字开头和结尾, 且只能包含字母、数字及分隔符.
-            </div>
-
-            <el-form-item label="容器配置" style="margin-top: 20px">
-              <el-button type="text" class="app-action-btn" @click="addContainer"
-                >增加容器</el-button
-              >
-            </el-form-item>
-
-            <el-form-item
-              v-for="(item, index) in data.deploymentForm.spec.template.spec.containers"
-              :key="index"
-              style="margin-top: -25px"
-            >
-              <el-card
-                style="
-                  width: 90%;
-                  height: 185px;
-                  background-color: #f2f2f2;
-                  margin-top: 20px;
-                  border-radius: 0px;
-                "
-              >
-                <div style="float: right; cursor: pointer" @click="deleteContainer(index)">
-                  <el-icon><Delete /></el-icon>
-                </div>
-                <el-col class="deploy-pixiu-column"
-                  >容器名称
-                  <el-input
-                    v-model="item.name"
-                    class="deploy-pixiu-incard"
-                    style="margin-left: 30px"
-                  />
-                </el-col>
-
-                <el-col style="margin-top: 10px" class="deploy-pixiu-column"
-                  >镜像
-                  <el-input
-                    v-model="item.image"
-                    style="margin-left: 56px"
-                    class="deploy-pixiu-incard"
-                  />
-                </el-col>
-
-                <el-col style="margin-top: 10px" class="deploy-pixiu-column"
-                  >拉取策略
-                  <el-radio-group v-model="item.imagePullPolicy" style="margin-left: 30px">
-                    <el-radio label="IfNotPresent" border>IfNotPresent</el-radio>
-                    <el-radio label="Always" border>Always</el-radio>
-                    <el-radio label="Never" border>Never</el-radio>
-                  </el-radio-group>
-                  <div class="container-line-describe">
-                    设置镜像拉取策略，默认使用 IfNotPresent 策略
-                  </div>
-                </el-col>
-              </el-card>
-            </el-form-item>
-
-            <el-form-item label="实例数量" style="margin-top: 20px">
-              <el-input-number
-                v-model="data.deploymentForm.spec.replicas"
-                style="margin-top: 8px"
-                :min="0"
-                size="small"
-                @change="handleChange"
-            /></el-form-item>
-            <div class="app-pixiu-line-describe" style="margin-top: -10px">Deployment 副本设置</div>
-
-            <div style="margin-top: 30px" />
-            <el-form-item style="margin-left: 30%">
-              <el-button class="pixiu-cancel-button" @click="cancelCreate()">取消</el-button>
-              <el-button class="pixiu-confirm-button" type="primary" @click="comfirmCreate()"
-                >确定</el-button
-              >
-            </el-form-item>
-          </el-form>
-        </el-card>
+      <el-form-item
+        v-for="(item, index) in data.deploymentLabels"
+        :key="index"
+        style="margin-top: -15px"
+      >
+        <div>
+          <el-input v-model="item.key" placeholder="标签键" style="width: 200px" />
+        </div>
+        <div style="margin-right: 10px; margin-left: 10px"></div>
+        <div>
+          <el-input v-model="item.value" placeholder="标签值" style="width: 200px" />
+        </div>
+        <div style="float: right; cursor: pointer; margin-left: 10px" @click="deleteLabel(index)">
+          <el-icon><Delete /></el-icon>
+        </div>
+      </el-form-item>
+      <div class="app-pixiu-line-describe">
+        标签键值以字母、数字开头和结尾, 且只能包含字母、数字及分隔符.
       </div>
-    </el-main>
-  </div>
+
+      <el-form-item label="容器配置" style="margin-top: 20px">
+        <el-button type="text" class="app-action-btn" @click="addContainer">增加容器</el-button>
+      </el-form-item>
+
+      <el-form-item
+        v-for="(item, index) in data.deploymentForm.spec.template.spec.containers"
+        :key="index"
+        style="margin-top: -25px"
+      >
+        <el-card
+          style="
+            width: 90%;
+            height: 185px;
+            background-color: #f2f2f2;
+            margin-top: 20px;
+            border-radius: 0px;
+          "
+        >
+          <div style="float: right; cursor: pointer" @click="deleteContainer(index)">
+            <el-icon><Delete /></el-icon>
+          </div>
+          <el-col class="deploy-pixiu-column"
+            >容器名称
+            <el-input v-model="item.name" class="deploy-pixiu-incard" style="margin-left: 30px" />
+          </el-col>
+
+          <el-col style="margin-top: 10px" class="deploy-pixiu-column"
+            >镜像
+            <el-input v-model="item.image" style="margin-left: 56px" class="deploy-pixiu-incard" />
+          </el-col>
+
+          <el-col style="margin-top: 10px" class="deploy-pixiu-column"
+            >拉取策略
+            <el-radio-group v-model="item.imagePullPolicy" style="margin-left: 30px">
+              <el-radio label="IfNotPresent" border>IfNotPresent</el-radio>
+              <el-radio label="Always" border>Always</el-radio>
+              <el-radio label="Never" border>Never</el-radio>
+            </el-radio-group>
+            <div class="container-line-describe">设置镜像拉取策略，默认使用 IfNotPresent 策略</div>
+          </el-col>
+        </el-card>
+      </el-form-item>
+
+      <el-form-item label="实例数量" style="margin-top: 20px">
+        <el-input-number
+          v-model="data.deploymentForm.spec.replicas"
+          style="margin-top: 8px"
+          :min="0"
+          size="small"
+          @change="handleChange"
+      /></el-form-item>
+      <div class="app-pixiu-line-describe" style="margin-top: -10px">Deployment 副本设置</div>
+
+      <div style="margin-top: 30px" />
+      <el-form-item style="margin-left: 30%">
+        <el-button class="pixiu-cancel-button" @click="cancelCreate()">取消</el-button>
+        <el-button class="pixiu-confirm-button" type="primary" @click="comfirmCreate()"
+          >确定</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script setup>
@@ -242,6 +220,7 @@ const cancelCreate = () => {
 
 onMounted(() => {
   data.cloud = proxy.$route.query;
+  data.cluster = data.cloud.cluster;
   data.path = proxy.$route.fullPath;
 
   getNamespaceList();
