170367a0fb222f58ec716be0d802ca0a8bda83b5
陈强 <cqiang1993@163.com>
2024-02-04 11:03:16 +0800

    添加校验解决问题 (#297)

--- src/page/container/service/createService.vue
+++ src/page/container/service/createService.vue
@@ -3,84 +3,161 @@
     <div class="font-container">创建 Service</div>
   </el-card>
 
-  <el-main>
-    <el-card class="create-card-style">
-      <el-form
-        ref="ruleFormRef"
-        :rules="rules"
-        label-position="left"
-        label-width="100px"
-        status-icon
-        :model="data.form"
-        require-asterisk-position="right"
-        style="margin-left: 3%; width: 80%"
+  <!-- 
+    本身就是在一个el-main下面了，没有特殊需求 
+    就不要再套一层el-main 
+    因为el-card下面引用了create-card-style的样式里面width=100%
+    导致了下面出现了滚动条 
+  -->
+  <!-- <el-main> -->
+  <el-card class="create-card-style">
+    <el-form
+      ref="ruleFormRef"
+      :rules="rules"
+      label-position="left"
+      label-width="100px"
+      status-icon
+      :model="data.form"
+      require-asterisk-position="right"
+      style="margin-left: 3%; width: 80%"
+    >
+      <div style="margin-top: 20px" />
+      <el-form-item label="名称" prop="metadata.name" style="width: 500px">
+        <el-input v-model="data.form.metadata.name" />
+        <div class="app-pixiu-line-describe2">
+          最长63个字符，只能包含小写字母、数字及分隔符("-")
+        </div>
+      </el-form-item>
+
+      <div style="margin-top: -10px" />
+      <el-form-item label="命名空间" style="width: 380px">
+        <div class="namespace-select-container">
+          <el-select v-model="data.form.metadata.namespace" @change="changeNamespace">
+            <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
+          </el-select>
+        </div>
+
+        <div style="margin-left: 10px; margin-top: 6px">
+          <pixiu-icon
+            name="icon-icon-refresh"
+            style="cursor: pointer"
+            size="16px"
+            type="iconfont"
+            color="#909399"
+            @click="getNamespaceList"
+          />
+        </div>
+      </el-form-item>
+
+      <div style="margin-top: 20px" />
+      <el-form-item label="类型" style="width: 600px">
+        <el-radio-group v-model="data.form.spec.type" style="margin-top: 4px">
+          <el-radio-button label="ClusterIP" border>ClusterIP</el-radio-button>
+          <el-radio-button label="NodePort" border>NodePort</el-radio-button>
+          <el-radio-button label="LoadBalancer" border>LoadBalancer</el-radio-button>
+        </el-radio-group>
+        <div class="app-pixiu-line-describe2">
+          <div v-if="data.form.spec.type === 'ClusterIP'">
+            通过集群的内部 IP 公开 Service，选择该值时 Service 只能够在集群内部访问。
+          </div>
+          <div v-else-if="data.form.spec.type === 'NodePort'">
+            通过每个节点上的 IP 和静态端口公开 Service
+          </div>
+          <div v-else>使用云平台的负载均衡器向外部公开 Service。</div>
+        </div>
+      </el-form-item>
+
+      <div style="margin-top: -6px" />
+      <el-form-item label="使用方式" style="width: 600px">
+        <el-radio-group v-model="data.serviceType" style="margin-top: 4px">
+          <el-radio-button label="常规服务" border>常规服务</el-radio-button>
+          <el-radio-button label="Headless" border>Headless</el-radio-button>
+        </el-radio-group>
+        <div class="app-pixiu-line-describe2">
+          <div v-if="data.serviceType === 'Headless'">
+            不需要负载均衡，也不需要单独的 Service IP。
+          </div>
+        </div>
+      </el-form-item>
+
+      <div style="margin-top: -10px" />
+      <el-form-item label="标签" style="margin-top: 10px">
+        <el-button type="text" class="app-action-btn" style="color: #3377ff" @click="addLabel"
+          >新增</el-button
+        >
+      </el-form-item>
+      <!-- 
+          el-form的model绑定的是data.form 
+          所以下面所有要经过表单校验的内容都必须在form里面，
+          原来的labels是直接写在data下的会导致校验不成功，需要调整 
+          其余的参考这个内容去实现
+        -->
+      <el-form-item
+        v-for="(item, index) in data.form.labels"
+        :key="index"
+        class="labels-item-style"
       >
-        <div style="margin-top: 20px" />
-        <el-form-item label="名称" prop="metadata.name" style="width: 500px">
-          <el-input v-model="data.form.metadata.name" />
-          <div class="app-pixiu-line-describe2">
-            最长63个字符，只能包含小写字母、数字及分隔符("-")
-          </div>
+        <el-form-item
+          :prop="'labels[' + index + '].key'"
+          :rules="[{ required: true, message: '标签键不能为空', trigger: 'blur' }]"
+        >
+          <el-input v-model="item.key" placeholder="标签键" style="width: 280px" />
         </el-form-item>
 
-        <div style="margin-top: -10px" />
-        <el-form-item label="命名空间" style="width: 380px">
-          <div class="namespace-select-container">
-            <el-select v-model="data.form.metadata.namespace" @change="changeNamespace">
-              <el-option v-for="item in data.namespaces" :key="item" :value="item" :label="item" />
-            </el-select>
-          </div>
+        <div style="margin-right: 10px; margin-left: 10px">=</div>
 
-          <div style="margin-left: 10px; margin-top: 6px">
-            <pixiu-icon
-              name="icon-icon-refresh"
-              style="cursor: pointer"
-              size="16px"
-              type="iconfont"
-              color="#909399"
-              @click="getNamespaceList"
-            />
-          </div>
+        <el-form-item
+          :prop="'labels[' + index + '].value'"
+          :rules="[{ required: true, message: '标签值不能为空', trigger: 'blur' }]"
+        >
+          <el-input v-model="item.value" placeholder="标签值" style="width: 280px" />
         </el-form-item>
 
-        <div style="margin-top: 20px" />
-        <el-form-item label="类型" style="width: 600px">
-          <el-radio-group v-model="data.form.spec.type" style="margin-top: 4px">
-            <el-radio-button label="ClusterIP" border>ClusterIP</el-radio-button>
-            <el-radio-button label="NodePort" border>NodePort</el-radio-button>
-            <el-radio-button label="LoadBalancer" border>LoadBalancer</el-radio-button>
-          </el-radio-group>
-          <div class="app-pixiu-line-describe2">
-            <div v-if="data.form.spec.type === 'ClusterIP'">
-              通过集群的内部 IP 公开 Service，选择该值时 Service 只能够在集群内部访问。
-            </div>
-            <div v-else-if="data.form.spec.type === 'NodePort'">
-              通过每个节点上的 IP 和静态端口公开 Service
-            </div>
-            <div v-else>使用云平台的负载均衡器向外部公开 Service。</div>
-          </div>
+        <div style="float: right; cursor: pointer; margin-left: 10px" @click="deleteLabel(index)">
+          <pixiu-icon
+            name="icon-shanchu"
+            size="14px"
+            type="iconfont"
+            style="margin-top: 10px; margin-left: 4px"
+            color="#909399"
+          />
+        </div>
+      </el-form-item>
+      <div class="app-pixiu-line-describe" style="margin-top: -5px">
+        标签键值以字母、数字开头和结尾, 且只能包含字母、数字及分隔符。
+      </div>
+
+      <div style="margin-top: 10px" />
+      <el-form-item label="Selector" style="width: 600px">
+        <el-radio-group v-model="data.selectorType" style="margin-top: 4px">
+          <el-radio-button label="普通" border>普通</el-radio-button>
+          <el-radio-button label="高级" border>高级</el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+
+      <div v-if="data.selectorType === '普通'">
+        <el-form-item style="margin-top: -8px">
+          <el-select v-model="data.deployment" style="width: 25%" @change="changeDeployment">
+            <el-option v-for="item in data.deployments" :key="item" :value="item" :label="item" />
+          </el-select>
         </el-form-item>
 
-        <div style="margin-top: -6px" />
-        <el-form-item label="使用方式" style="width: 600px">
-          <el-radio-group v-model="data.serviceType" style="margin-top: 4px">
-            <el-radio-button label="常规服务" border>常规服务</el-radio-button>
-            <el-radio-button label="Headless" border>Headless</el-radio-button>
-          </el-radio-group>
-          <div class="app-pixiu-line-describe2">
-            <div v-if="data.serviceType === 'Headless'">
-              不需要负载均衡，也不需要单独的 Service IP。
-            </div>
-          </div>
-        </el-form-item>
+        <div class="app-pixiu-line-describe" style="margin-top: -10px">
+          选择 Service 需要关联的后端 workload。
+        </div>
+      </div>
 
-        <div style="margin-top: -10px" />
-        <el-form-item label="标签" style="margin-top: 10px">
-          <el-button type="text" class="app-action-btn" style="color: #3377ff" @click="addLabel"
+      <div v-if="data.selectorType === '高级'">
+        <el-form-item style="margin-top: -15px">
+          <el-button type="text" class="app-action-btn" style="color: #3377ff" @click="addSelector"
             >新增</el-button
           >
         </el-form-item>
-        <el-form-item v-for="(item, index) in data.labels" :key="index" style="margin-top: -15px">
+        <el-form-item
+          v-for="(item, index) in data.selectors"
+          :key="index"
+          style="margin-top: -15px"
+        >
           <div>
             <el-input v-model="item.key" placeholder="标签键" style="width: 280px" />
           </div>
@@ -88,7 +165,10 @@
           <div>
             <el-input v-model="item.value" placeholder="标签值" style="width: 280px" />
           </div>
-          <div style="float: right; cursor: pointer; margin-left: 10px" @click="deleteLabel(index)">
+          <div
+            style="float: right; cursor: pointer; margin-left: 10px"
+            @click="deleteSelector(index)"
+          >
             <pixiu-icon
               name="icon-shanchu"
               size="14px"
@@ -99,135 +179,73 @@
           </div>
         </el-form-item>
         <div class="app-pixiu-line-describe" style="margin-top: -10px">
-          标签键值以字母、数字开头和结尾, 且只能包含字母、数字及分隔符。
+          Selector 以字母、数字开头和结尾, 且只能包含字母、数字及分隔符。
         </div>
+      </div>
 
-        <div style="margin-top: 10px" />
-        <el-form-item label="Selector" style="width: 600px">
-          <el-radio-group v-model="data.selectorType" style="margin-top: 4px">
-            <el-radio-button label="普通" border>普通</el-radio-button>
-            <el-radio-button label="高级" border>高级</el-radio-button>
-          </el-radio-group>
-        </el-form-item>
+      <el-form-item label="Ports" style="margin-top: 20px">
+        <div class="label-title-style" style="font-size: 13px">服务端口</div>
+        <div class="label-title-style" style="margin-left: 148px; font-size: 13px">协议</div>
+        <div class="label-title-style" style="margin-left: 175px; font-size: 13px">目标端口</div>
+        <div class="label-title-style" style="margin-left: 146px; font-size: 13px">端口名</div>
 
-        <div v-if="data.selectorType === '普通'">
-          <el-form-item style="margin-top: -8px">
-            <el-select v-model="data.deployment" style="width: 25%" @change="changeDeployment">
-              <el-option v-for="item in data.deployments" :key="item" :value="item" :label="item" />
-            </el-select>
-          </el-form-item>
+        <el-divider style="width: 85%; margin-top: 2px" />
 
-          <div class="app-pixiu-line-describe" style="margin-top: -10px">
-            选择 Service 需要关联的后端 workload。
-          </div>
-        </div>
-
-        <div v-if="data.selectorType === '高级'">
-          <el-form-item style="margin-top: -15px">
-            <el-button
-              type="text"
-              class="app-action-btn"
-              style="color: #3377ff"
-              @click="addSelector"
-              >新增</el-button
-            >
-          </el-form-item>
-          <el-form-item
-            v-for="(item, index) in data.selectors"
-            :key="index"
-            style="margin-top: -15px"
+        <el-form-item style="margin-top: -15px">
+          <el-button type="text" class="app-action-btn" style="color: #3377ff" @click="addPort"
+            >新增</el-button
           >
-            <div>
-              <el-input v-model="item.key" placeholder="标签键" style="width: 280px" />
-            </div>
-            <div style="margin-right: 10px; margin-left: 10px">=</div>
-            <div>
-              <el-input v-model="item.value" placeholder="标签值" style="width: 280px" />
-            </div>
-            <div
-              style="float: right; cursor: pointer; margin-left: 10px"
-              @click="deleteSelector(index)"
-            >
-              <pixiu-icon
-                name="icon-shanchu"
-                size="14px"
-                type="iconfont"
-                style="margin-top: 10px; margin-left: 4px"
-                color="#909399"
-              />
-            </div>
-          </el-form-item>
-          <div class="app-pixiu-line-describe" style="margin-top: -10px">
-            Selector 以字母、数字开头和结尾, 且只能包含字母、数字及分隔符。
+        </el-form-item>
+
+        <el-form-item
+          v-for="(item, index) in data.form.spec.ports"
+          :key="index"
+          style="margin-top: -10px"
+        >
+          <div>
+            <el-input v-model="item.port" placeholder="1-65535内的整数" style="width: 180px" />
           </div>
-        </div>
-
-        <el-form-item label="Ports" style="margin-top: 20px">
-          <div class="label-title-style" style="font-size: 13px">服务端口</div>
-          <div class="label-title-style" style="margin-left: 148px; font-size: 13px">协议</div>
-          <div class="label-title-style" style="margin-left: 175px; font-size: 13px">目标端口</div>
-          <div class="label-title-style" style="margin-left: 146px; font-size: 13px">端口名</div>
-
-          <el-divider style="width: 85%; margin-top: 2px" />
-
-          <el-form-item style="margin-top: -15px">
-            <el-button type="text" class="app-action-btn" style="color: #3377ff" @click="addPort"
-              >新增</el-button
-            >
-          </el-form-item>
-
-          <el-form-item
-            v-for="(item, index) in data.form.spec.ports"
-            :key="index"
-            style="margin-top: -10px"
-          >
-            <div>
-              <el-input v-model="item.port" placeholder="1-65535内的整数" style="width: 180px" />
-            </div>
-            <div style="margin-left: 20px">
-              <el-input v-model="item.protocol" placeholder="TCP/UDP" style="width: 180px" />
-            </div>
-            <div style="margin-left: 20px">
-              <el-input
-                v-model="item.targetPort"
-                placeholder="1-65535内的整数"
-                style="width: 180px"
-              />
-            </div>
-            <div style="margin-left: 20px">
-              <el-input
-                v-model="item.name"
-                placeholder="1-63位小写字母、数字或下划线组成"
-                style="width: 250px"
-              />
-            </div>
-            <div
-              style="float: right; cursor: pointer; margin-left: 10px"
-              @click="deletePort(index)"
-            >
-              <pixiu-icon
-                name="icon-shanchu"
-                size="14px"
-                type="iconfont"
-                style="margin-top: 10px; margin-left: 4px"
-                color="#909399"
-              />
-            </div>
-          </el-form-item>
+          <div style="margin-left: 20px">
+            <el-input v-model="item.protocol" placeholder="TCP/UDP" style="width: 180px" />
+          </div>
+          <div style="margin-left: 20px">
+            <el-input
+              v-model="item.targetPort"
+              placeholder="1-65535内的整数"
+              style="width: 180px"
+            />
+          </div>
+          <div style="margin-left: 20px">
+            <el-input
+              v-model="item.name"
+              placeholder="1-63位小写字母、数字或下划线组成"
+              style="width: 250px"
+            />
+          </div>
+          <div style="float: right; cursor: pointer; margin-left: 10px" @click="deletePort(index)">
+            <pixiu-icon
+              name="icon-shanchu"
+              size="14px"
+              type="iconfont"
+              style="margin-top: 10px; margin-left: 4px"
+              color="#909399"
+            />
+          </div>
         </el-form-item>
+      </el-form-item>
 
-        <el-form-item label="会话保持" style="margin-top: 20px">
-          <el-switch v-model="data.Session" inline-prompt width="40px" />
-        </el-form-item>
+      <el-form-item label="会话保持" style="margin-top: 20px">
+        <el-switch v-model="data.Session" inline-prompt width="40px" />
+      </el-form-item>
 
-        <div style="margin-top: 30px" />
-        <el-form-item style="margin-left: 30%">
-          <el-button class="pixiu-cancel-button" @click="cancel()">取消</el-button>
-          <el-button class="pixiu-confirm-button" type="primary" @click="comfirm()">确定</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
-  </el-main>
+      <div style="margin-top: 30px" />
+      <el-form-item style="margin-left: 30%">
+        <el-button class="pixiu-cancel-button" @click="cancel()">取消</el-button>
+        <el-button class="pixiu-confirm-button" type="primary" @click="comfirm()">确定</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <!-- </el-main> -->
 </template>
 
 <script setup>
@@ -254,6 +272,7 @@ const data = reactive({
       name: '',
       namespace: 'default',
     },
+
     spec: {
       ports: [
         {
@@ -266,6 +285,7 @@ const data = reactive({
       selector: {},
       type: 'ClusterIP',
     },
+    labels: [],
   },
 
   deployment: '',
@@ -413,14 +433,14 @@ const getNamespaceList = async () => {
 };
 
 const addLabel = () => {
-  data.labels.push({
+  data.form.labels.push({
     key: '',
     value: '',
   });
 };
 
 const deleteLabel = (index) => {
-  data.labels.splice(index, 1);
+  data.form.labels.splice(index, 1);
 };
 
 const addSelector = () => {
@@ -459,5 +479,18 @@ const backToService = () => {
 .label-title-style {
   font-size: 12px;
   color: #888888;
+}
+/*
+  将第一个和最后一个元素的圆角变成0
+*/
+.create-card-style .el-radio-button:first-child .el-radio-button__inner {
+  border-radius: 0;
+}
+.create-card-style .el-radio-button:last-child .el-radio-button__inner {
+  border-radius: 0;
+}
+.create-card-style .labels-item-style {
+  display: flex;
+  margin-top: 15px;
 }
 </style>
