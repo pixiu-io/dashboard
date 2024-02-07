be904c36aca99ea318814c41f67a0133208fab69
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-07 09:13:10 +0800

    实现deployment的yaml编辑 (#307)

--- src/services/kubernetes/deploymentService.js
+++ src/services/kubernetes/deploymentService.js
@@ -1,13 +1,34 @@
 import http from '@/utils/http';
 import { awaitWrap } from '@/utils/utils';
 
-export const createDeployment = async (data, cloud_name, namespace, name) => {
+export const createDeployment = async (cluster, namespace, name, data) => {
   const [err, result] = await awaitWrap(
     http({
       method: 'post',
-      url: `/clouds/apps/v1/${cloud_name}/namespaces/${namespace}/deployments/${name}`,
+      url: `/proxy/pixiu/${cluster}/apis/apps/v1/namespaces/${namespace}/deployments/${name}`,
       data,
     }),
   );
-  return [err, result];
+  return [result, err];
+};
+
+export const getDeployment = async (cluster, namespace, name) => {
+  const [err, result] = await awaitWrap(
+    http({
+      method: 'get',
+      url: `/proxy/pixiu/${cluster}/apis/apps/v1/namespaces/${namespace}/deployments/${name}`,
+    }),
+  );
+  return [result, err];
+};
+
+export const updateDeployment = async (cluster, namespace, name, data) => {
+  const [err, result] = await awaitWrap(
+    http({
+      method: 'put',
+      url: `/proxy/pixiu/${cluster}/apis/apps/v1/namespaces/${namespace}/deployments/${name}`,
+      data,
+    }),
+  );
+  return [result, err];
 };
