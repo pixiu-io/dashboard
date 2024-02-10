2f71392a30bc958734270020ccfa6c9f38318e55
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-10 15:34:43 +0800

    封装删除的dialog (#315)

---
+++ src/components/pixiuDialog/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-dialog
+    :model-value="closeEvent"
+    style="color: #000000; font: 14px; height: 190px"
+    width="500px"
+    align-center
+    center
+    @close="cancel"
+  >
+    <template #header>
+      <div
+        style="
+          text-align: left;
+          font-weight: bold;
+          padding-left: 5px;
+          margin-top: 5px;
+          font-size: 14.5px;
+        "
+      >
+        删除{{ objectName }}
+      </div>
+    </template>
+
+    <div style="margin-top: -6px; font-weight: bold; font-size: 13.5px; color: #000">
+      此操作将永久删除{{ objectName }} (<span style="color: red">{{ deleteName }}</span
+      >) 是否继续？
+    </div>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button class="pixiu-delete-cancel-button" @click="cancel">取消</el-button>
+        <el-button type="primary" class="pixiu-delete-confirm-button" @click="confirm"
+          >确认</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+defineProps({
+  closeEvent: { type: Boolean, default: false },
+  objectName: { type: String, default: '' },
+  deleteName: { type: String, default: '' },
+});
+
+const emit = defineEmits(['confirm', 'cancel']);
+
+const confirm = () => {
+  emit('confirm');
+};
+
+const cancel = () => {
+  emit('cancel');
+};
+</script>
+
+<style scoped></style>
