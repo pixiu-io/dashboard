f6bd6e71cb9853e7041eb1286841d2702384cb38
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-10 20:14:15 +0800

    add

--- src/page/container/pod/createPod.vue
+++ src/page/container/pod/createPod.vue
@@ -1,1 +1,83 @@
-<template>create pod</template>
+<template>
+  <el-card class="title-card-container">
+    <div class="font-container">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item @click="backToPod"
+          ><span style="color: black; cursor: pointer"> Pod </span>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item style="color: black">{{ data.cluster }}</el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <span style="color: black"> 创建 Pod </span>
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+  </el-card>
+</template>
+
+<script setup>
+import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
+import { getNamespaceNames } from '@/services/kubernetes/namespaceService';
+
+const { proxy } = getCurrentInstance();
+
+const data = reactive({
+  loading: false,
+
+  clutser: '',
+  namespaces: [],
+
+  form: {
+    metadata: {
+      name: '',
+      namespace: 'default',
+    },
+
+    spec: {},
+  },
+});
+
+const rules = {
+  'metadata.name': [{ required: true, message: '请输入 Service 名称', trigger: 'blur' }],
+};
+
+onMounted(() => {
+  data.query = proxy.$route.query;
+  data.cluster = data.query.cluster;
+
+  data.path = proxy.$route.fullPath;
+
+  getNamespaces();
+});
+
+watch(
+  () => data.selectorType,
+  (newActive, oldActive) => {},
+);
+
+const cancel = () => {
+  backToService();
+};
+
+const changeNamespace = async (val) => {
+  localStorage.setItem('namespace', val);
+  data.form.metadata.namespace = val;
+};
+
+const getNamespaces = async () => {
+  const [result, err] = await getNamespaceNames(data.cluster);
+  if (err) {
+    proxy.$message.error(err.response.data.message);
+    return;
+  }
+  data.namespaces = result;
+};
+
+const backToPod = () => {
+  proxy.$router.push({
+    name: 'Pod',
+    query: data.query,
+  });
+};
+</script>
+
+<style></style>
