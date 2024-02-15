4e2f0f0429f8b733e35410e455252799ec11b08b
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-15 17:56:12 +0800

    add

--- src/page/container/node/nodeDetail.vue
+++ src/page/container/node/nodeDetail.vue
@@ -1,16 +1,16 @@
 <template>
-  <div style="display: flex; flex-direction: column; width: 100%; height: 100%">
-    <div style="width: 100%; height: 50px; background: #ffffff; display: flex; align-items: center">
+  <el-card style="display: flex; flex-direction: column; width: 100%; height: 50px">
+    <div style="width: 100%; background: #ffffff; display: flex; align-items: center">
       <pixiu-icon
         name="icon-back"
-        style="cursor: pointer; margin-left: 25px"
+        style="cursor: pointer; margin-left: 4px"
         size="16px"
         type="iconfont"
         color="#006eff"
         @click="goToNode"
       />
 
-      <el-breadcrumb separator="/" style="margin-left: 10px; margin-top: 1px">
+      <el-breadcrumb separator="/" style="margin-left: 10px">
         <el-breadcrumb-item
           ><span class="breadcrumb-create-style"> 集群 </span>
         </el-breadcrumb-item>
@@ -25,15 +25,11 @@
         </el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-  </div>
+  </el-card>
 </template>
 
 <script setup lang="jsx">
-import { useRouter } from 'vue-router';
 import { reactive, getCurrentInstance, onMounted, ref } from 'vue';
-import { formatTimestamp } from '@/utils/utils';
-import useClipboard from 'vue-clipboard3';
-import { ElMessage } from 'element-plus';
 
 const { proxy } = getCurrentInstance();
 
@@ -41,8 +37,6 @@ const data = reactive({
   name: '',
   clusterName: '',
   cluster: '',
-
-  activeName: 'five',
 });
 
 onMounted(async () => {
