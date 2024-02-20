c31cea8e492257a32e96899ae57e788ed6f4c896
caoyingjunz <cao.yingjunz@gmail.com>
2024-02-20 21:15:18 +0800

    实现详情页面

--- src/page/container/node/nodeDetail.vue
+++ src/page/container/node/nodeDetail.vue
@@ -1,35 +1,37 @@
 <template>
-  <el-card style="display: flex; flex-direction: column; width: 100%; height: 55px">
-    <div style="width: 100%; background: #ffffff; display: flex; align-items: center">
-      <pixiu-icon
-        name="icon-back"
-        style="cursor: pointer; margin-left: 4px"
-        size="16px"
-        type="iconfont"
-        color="#006eff"
-        @click="goToNode"
-      />
+  <div style="width: 100%">
+    <el-card class="detail-card-style-header">
+      <div style="width: 100%; background: #ffffff; display: flex; align-items: center">
+        <pixiu-icon
+          name="icon-back"
+          style="cursor: pointer; margin-left: 4px"
+          size="16px"
+          type="iconfont"
+          color="#006eff"
+          @click="goToNode"
+        />
 
-      <el-breadcrumb separator="/" style="margin-left: 10px">
-        <el-breadcrumb-item
-          ><span class="breadcrumb-create-style"> 集群 </span>
-        </el-breadcrumb-item>
-        <el-breadcrumb-item
-          ><span class="breadcrumb-create-style"> {{ data.clusterName }} </span>
-        </el-breadcrumb-item>
-        <el-breadcrumb-item
-          ><span class="breadcrumb-create-style"> Node </span>
-        </el-breadcrumb-item>
-        <el-breadcrumb-item
-          ><span class="breadcrumb-create-style"> Node详情 </span>
-        </el-breadcrumb-item>
-      </el-breadcrumb>
-    </div>
-  </el-card>
+        <el-breadcrumb separator="/" style="margin-left: 10px">
+          <el-breadcrumb-item
+            ><span class="breadcrumb-create-style"> {{ data.clusterName }} </span>
+          </el-breadcrumb-item>
+          <el-breadcrumb-item
+            ><span class="breadcrumb-create-style"> Node </span>
+          </el-breadcrumb-item>
+          <el-breadcrumb-item
+            ><span class="breadcrumb-create-style"> Node详情 </span>
+          </el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+    </el-card>
+
+    <div class="detail-card-style-body">ddd</div>
+  </div>
 </template>
 
 <script setup lang="jsx">
 import { reactive, getCurrentInstance, onMounted, ref } from 'vue';
+import { getNode } from '@/services/kubernetes/nodeService';
 
 const { proxy } = getCurrentInstance();
 
@@ -37,6 +39,8 @@ const data = reactive({
   name: '',
   clusterName: '',
   cluster: '',
+
+  nodeObject: '',
 });
 
 onMounted(async () => {
@@ -44,12 +48,17 @@ onMounted(async () => {
   data.clusterName = localStorage.getItem(data.cluster);
   data.name = proxy.$route.query.name;
 
-  await GetNode();
+  await getNodeObject();
 });
 
-const GetNode = async () => {
-  try {
-  } catch (error) {}
+const getNodeObject = async () => {
+  const [res, err] = await getNode(data.cluster, data.name);
+  if (err) {
+    proxy.$notify.error({ title: 'Node', message: err.response.data.message });
+    return;
+  }
+
+  data.nodeObject = res;
 };
 
 const goToNode = () => {
