8b09c758952c60fdee8b6510c45fa74b294496cc
caoyingjunz <cao.yingjunz@gmail.com>
2024-03-27 00:03:39 +0800

    Revert "add"
    This reverts commit 6857b9c5a347bd95679784abe4adb679746473b1.

--- src/page/container/pod/pod.vue
+++ src/page/container/pod/pod.vue
@@ -160,6 +160,39 @@
     @confirm="confirm"
     @cancel="cancel"
   ></pixiuDialog>
+
+  <el-dialog
+    :model-value="data.remoteLogin.close"
+    style="color: #000000; font: 14px"
+    width="500px"
+    align-center
+    center
+    @close="cancelRemoteLogin"
+  >
+    <template #header>
+      <div
+        style="
+          text-align: left;
+          font-weight: bold;
+          padding-left: 5px;
+          margin-top: 5px;
+          font-size: 14.5px;
+          color: #191919;
+        "
+      >
+        远程登录
+      </div>
+    </template>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button class="pixiu-delete-cancel-button" @click="cancelRemoteLogin">取消</el-button>
+        <el-button type="primary" class="pixiu-delete-confirm-button" @click="confirmRemoteLogin"
+          >确认</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="jsx">
@@ -218,6 +251,10 @@ const data = reactive({
     objectName: 'Pod',
     deleteName: '',
   },
+
+  remoteLogin: {
+    close: false,
+  },
 });
 
 const onChange = (v) => {
@@ -246,6 +283,16 @@ const createPod = () => {
 const handleDeleteDialog = (row) => {
   data.deleteDialog.close = true;
   data.deleteDialog.deleteName = row.metadata.name;
+};
+
+const cancelRemoteLogin = () => {
+  data.remoteLogin.close = false;
+};
+
+const confirmRemoteLogin = () => {};
+
+const handleRemoteLoginDialog = (row) => {
+  data.remoteLogin.close = true;
 };
 
 const confirm = async () => {
