52175ed7e2922e7b89036c3fe582c35f29dbc17a
Gitlayzer <18037803502@163.com>
2024-03-30 20:57:50 +0800

    Roll back Pod changes

--- src/page/container/pod/pod.vue
+++ src/page/container/pod/pod.vue
@@ -189,7 +189,6 @@
         style="vertical-align: middle; font-size: 16px; margin-left: -25px; margin-top: -50px"
         ><WarningFilled
       /></el-icon>
-      
       <div style="vertical-align: middle; margin-top: -40px">基于 WebShell 提供登陆容器的功能</div>
     </el-card>
 
