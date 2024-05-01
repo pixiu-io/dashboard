db83bd1fc92efb6a279db8a5e4683ac3d99d5fa6
caoyingjunz <cao.yingjunz@gmail.com>
2024-05-01 18:30:09 +0800

    完善网络信息页面 (#479)

--- src/page/container/info/info.vue
+++ src/page/container/info/info.vue
@@ -131,6 +131,81 @@
         </el-form-item>
       </div>
     </el-card>
+
+    <el-card class="content2-card-container" style="height: 250px">
+      <div
+        style="
+          text-align: left;
+          font-weight: bold;
+          padding-left: 5px;
+          margin-top: 2px;
+          font-size: 14.5px;
+          color: #191919;
+        "
+      >
+        网络信息
+      </div>
+
+      <div style="margin-top: 8px; width: 100%; border-radius: 0px">
+        <el-form-item>
+          <template #label>
+            <span style="margin-left: 6px; font-size: 13px; color: #606266">CNI</span>
+          </template>
+          <span class="detail-card-style-form2" style="margin-left: 96px">
+            <el-tag type="primary">flannel</el-tag>
+          </span>
+        </el-form-item>
+      </div>
+
+      <div style="margin-top: -12px"></div>
+      <el-form-item>
+        <template #label>
+          <span style="margin-left: 6px; font-size: 13px; color: #606266">服务网络</span>
+        </template>
+        <span class="detail-card-style-form2" style="margin-left: 67px">
+          {{ data.configData.networking.serviceSubnet }}
+        </span>
+      </el-form-item>
+
+      <div style="margin-top: -12px"></div>
+      <el-form-item>
+        <template #label>
+          <span style="margin-left: 6px; font-size: 13px; color: #606266">容器网络</span>
+        </template>
+        <span class="detail-card-style-form2" style="margin-left: 67px">
+          {{ data.configData.networking.podSubnet }}
+        </span>
+      </el-form-item>
+
+      <div style="margin-top: -12px"></div>
+      <el-form-item>
+        <template #label>
+          <span style="margin-left: 6px; font-size: 13px; color: #606266">DNS域</span>
+        </template>
+        <span class="detail-card-style-form2" style="margin-left: 80px">
+          {{ data.configData.networking.dnsDomain }}
+        </span>
+        <div style="margin-left: 8px; cursor: pointer">
+          <pixiu-icon
+            name="icon-copy"
+            size="12px"
+            type="iconfont"
+            color="#909399"
+            @click="copy(data.configData.networking.dnsDomain)"
+          />
+        </div>
+      </el-form-item>
+
+      <div style="margin-top: -12px"></div>
+      <el-form-item>
+        <template #label>
+          <span style="margin-left: 6px; font-size: 13px; color: #606266">转发模式</span>
+        </template>
+        <span class="detail-card-style-form2" style="margin-left: 68px">
+          {{ data.configData.mode }}
+        </span>
+      </el-form-item>
+    </el-card>
   </dev>
 </template>
 
@@ -139,9 +214,8 @@ import { reactive, getCurrentInstance, onMounted, watch, ref } from 'vue';
 import PixiuCard from '@/components/card/index.vue';
 import { getClustersById } from '@/services/cloudService';
 import { runningFormatter } from '@/utils/formatter';
-import { getConfigMap } from '@/services/kubernetes/configmapService';
+import { getConfigMapContent } from '@/services/kubernetes/configmapService';
 import { copy } from '@/utils/utils';
-import jsYaml from 'js-yaml';
 
 const { proxy } = getCurrentInstance();
 const ruleFormRef = ref();
@@ -161,6 +235,7 @@ const data = reactive({
   configData: {
     controlPlaneEndpoint: '',
     networking: {},
+    mode: 'iptables',
   },
 });
 
@@ -170,6 +245,7 @@ onMounted(() => {
 
   getCluster();
   GetConfigMap();
+  GetProxyConfig();
 });
 
 const getCluster = async () => {
@@ -181,17 +257,34 @@ const getCluster = async () => {
 };
 
 const GetConfigMap = async () => {
-  const [result, err] = await getConfigMap(data.cluster, 'kube-system', 'kubeadm-config');
+  const [cfg, err] = await getConfigMapContent(
+    data.cluster,
+    'kube-system',
+    'kubeadm-config',
+    'ClusterConfiguration',
+  );
   if (err) {
-    proxy.$notify.error(err.response.data.message);
+    proxy.$notify.error(err);
     return;
   }
+  data.configData.controlPlaneEndpoint = 'https://' + cfg.controlPlaneEndpoint;
+  data.configData.networking = cfg.networking;
+};
 
-  const cd = result.data.ClusterConfiguration;
-  const jsData = jsYaml.load(cd);
-
-  data.configData.controlPlaneEndpoint = 'https://' + jsData.controlPlaneEndpoint;
-  data.configData.networking = jsData.networking;
+const GetProxyConfig = async () => {
+  const [cfg, err] = await getConfigMapContent(
+    data.cluster,
+    'kube-system',
+    'kube-proxy',
+    'config.conf',
+  );
+  if (err) {
+    proxy.$notify.error(err);
+    return;
+  }
+  if (cfg.mode !== 'iptables' && cfg.mode !== '') {
+    ata.configData.mode = cfg.mode;
+  }
 };
 </script>
 
