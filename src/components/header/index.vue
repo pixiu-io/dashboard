860b0fb61e66541a30ba582a833e5394410a0746
caoyingjunz <cao.yingjunz@gmail.com>
2024-05-29 22:54:05 +0800

    支持用户退出登陆 (#509)

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -144,6 +144,7 @@
 
 <script setup>
 import { reactive, ref, getCurrentInstance, onMounted } from 'vue';
+import { logoutMethod } from '@/services/loginService';
 
 const { proxy } = getCurrentInstance();
 const data = reactive({
@@ -169,6 +170,9 @@ const handleMessage = () => {
 const logout = () => {
   // 清除本地缓存的 token 和 account
   localStorage.clear();
+  // 直接发送退出请求，忽略是否真的已退出
+  logoutMethod(data.userId);
+
   // 跳转到登陆页面
   proxy.$router.push('/login');
 };
