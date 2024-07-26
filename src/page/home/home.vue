51ebd096dcdbe3bf693dd47345a279a1ac391c58
caoyingjunz <cao.yingjunz@gmail.com>
2024-07-27 00:19:52 +0800

    add

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,17 +1,20 @@
 <template>
   <el-main style="background-color: #f3f4f7">
     <el-row :gutter="15" style="height: 100%">
-      <el-col :span="20">
-        <el-row style="height: 49%">
-          <el-card style="width: 100%; height: 100%; border-radius: 0px">123</el-card>
+      <el-col :span="18">
+        <el-row style="height: 58%">
+          <el-card style="width: 100%; height: 100%; border-radius: 0px">基本信息</el-card>
         </el-row>
+
         <el-row style="height: 2%"></el-row>
-        <el-row style="height: 49%">
-          <el-card style="width: 100%; height: 100%; border-radius: 0px">123</el-card></el-row
+
+        <el-row style="height: 40%">
+          <el-card style="width: 100%; height: 100%; border-radius: 0px">监控大盘</el-card></el-row
         >
       </el-col>
-      <el-col :span="4">
-        <el-card style="width: 100%; height: 100%; border-radius: 0px">123</el-card></el-col
+
+      <el-col :span="6">
+        <el-card style="width: 100%; height: 100%; border-radius: 0px">操作记录</el-card></el-col
       >
     </el-row>
   </el-main>
