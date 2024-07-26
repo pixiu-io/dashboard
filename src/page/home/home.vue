b6a2a49f54f93e6088783e746b76ec135488ddaa
caoyingjunz <cao.yingjunz@gmail.com>
2024-07-26 23:50:47 +0800

    add

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,7 +1,40 @@
-<template></template>
+<template>
+  <div style="display: flex">
+    <div>
+      <el-card class="content1-card-container"> dddddddddddddd</el-card>
+    </div>
+    <div>
+      <el-card class="content3-card-container"> ddd</el-card>
+    </div>
+  </div>
+</template>
 
-<script setup>
-import { reactive } from 'vue';
-</script>
+<style>
+.content1-card-container {
+  margin-top: 20px;
+  margin-left: 20px;
+  border-radius: 0px;
+}
 
-<style scoped></style>
+.content2-card-container {
+  height: 329px;
+  margin-top: 5px;
+  margin-left: 10px;
+  margin-right: 10px;
+  border-radius: 0px;
+}
+
+.content3-card-container {
+  margin-top: 20px;
+  margin-left: 20px;
+  border-radius: 0px;
+}
+
+.content4-card-container {
+  height: 140px;
+  width: 96%;
+  margin-top: 25px;
+  margin-left: 10px;
+  border-radius: 0px;
+}
+</style>
