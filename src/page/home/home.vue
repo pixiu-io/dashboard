e872146c94636da288e0780b368ec71b3a78a93a
caoyingjunz <cao.yingjunz@gmail.com>
2024-07-27 01:24:22 +0800

    add

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -3,7 +3,11 @@
     <el-row :gutter="15" style="height: 100%">
       <el-col :span="18">
         <el-row style="height: 58%">
-          <el-card style="width: 100%; height: 100%; border-radius: 0px">基本信息</el-card>
+          <el-card style="width: 100%; height: 100%; border-radius: 0px">
+            <div style="width: 45%; margin-left: 1px; margin-top: 1px; display: inline-block">
+              <my-echarts :option="planOption"></my-echarts>
+            </div>
+          </el-card>
         </el-row>
 
         <el-row style="height: 2%"></el-row>
@@ -19,5 +23,44 @@
     </el-row>
   </el-main>
 </template>
+
+<script setup>
+import { reactive } from 'vue';
+import MyEcharts from '@/components/echarts/index.vue';
+
+const planOption = reactive({
+  title: { text: '部署计划' },
+  legend: {
+    top: 'bottom',
+  },
+  toolbox: {
+    show: false,
+    feature: {
+      mark: { show: true },
+      dataView: { show: true, readOnly: false },
+      restore: { show: true },
+      saveAsImage: { show: true },
+    },
+  },
+  series: [
+    {
+      name: '产品使用分析',
+      type: 'pie',
+      radius: [20, 140],
+      center: ['50%', '50%'],
+      roseType: 'area',
+      itemStyle: {
+        borderRadius: 8,
+      },
+      data: [
+        { value: 40, name: '部署中' },
+        { value: 38, name: '运行中' },
+        { value: 32, name: '未开始' },
+        { value: 10, name: '部署失败' },
+      ],
+    },
+  ],
+});
+</script>
 
 <style></style>
