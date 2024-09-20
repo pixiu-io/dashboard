8a46199d83f4cc2e41d5e8e3fc6b6ee9754aa584
caoyingjunz <cao.yingjunz@gmail.com>
2024-09-21 00:59:55 +0800

    修复域名白名单更新不显示问题

--- src/page/container/ingress/ingress.vue
+++ src/page/container/ingress/ingress.vue
@@ -475,6 +475,22 @@ const handleWhiteListDialog = (row) => {
   data.whiteListData.namespace = row.metadata.namespace;
   data.whiteListData.name = row.metadata.name;
 
+  if (row.metadata.annotations) {
+    const snippet = row.metadata.annotations['nginx.ingress.kubernetes.io/server-snippet'];
+    if (snippet) {
+      data.whiteListData.enable = true;
+      data.whiteListData.allowIps = [];
+
+      let parts = snippet.split('\n');
+      parts.pop();
+      for (let p of parts) {
+        const result = p.slice(0, -1);
+        data.whiteListData.allowIps.push(result.split('allow ').join(''));
+        data.whiteListData.ipList = data.whiteListData.allowIps.join(',');
+      }
+    }
+  }
+
   data.whiteListData.close = true;
 };
 
@@ -482,6 +498,7 @@ const cancelWhiteList = () => {
   data.whiteListData.close = false;
   setTimeout(() => {
     data.whiteListData.enable = false;
+    data.whiteListData.ipList = '';
     data.whiteListData.allowIps = [];
     data.whiteListData.namespace = '';
     data.whiteListData.name = '';
@@ -526,6 +543,7 @@ const confirmWhiteList = async () => {
 
   proxy.$notify.success('白名单设置完成');
   cancelWhiteList();
+  getIngresses();
 };
 // 白名单结束
 
