4d497480c5f196792c03eb07e24a5ac8df10e649
caoyingjunz <cao.yingjunz@gmail.com>
2024-09-30 23:45:49 +0800

    支持 delete cronjob 接口

--- src/services/kubernetes/cronjobService.js
+++ src/services/kubernetes/cronjobService.js
@@ -18,6 +18,16 @@ export const getCronJobList = async (cluster, namespace) => {
   return [result, err];
 };
 
+export const deleteCronJob = async (cluster, namespace, name) => {
+  const [err, result] = await awaitWrap(
+    http({
+      method: 'delete',
+      url: `/pixiu/proxy/${cluster}/apis/batch/v1/namespaces/${namespace}/cronjobs/${name}`,
+    }),
+  );
+  return [result, err];
+};
+
 export const patchCronJob = async (cluster, namespace, name, data) => {
   const [err, result] = await awaitWrap(
     http({
