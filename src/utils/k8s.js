d6bcbf7c22b2d8df54dc9b8eb6025a366a9ea8df
caoyingjunz <cao.yingjunz@gmail.com>
2024-10-27 17:26:00 +0800

    add

--- src/utils/k8s.js
+++ src/utils/k8s.js
@@ -11,69 +11,68 @@ const makePodTemplate = (data) => {
     }
     tplSpec['nodeSelector'] = nodeSelects;
   }
-};
 
-let targetContainers = [];
-for (let fc of data.containers) {
-  let targetContainer = {
-    name: fc.name,
-    image: fc.image,
-    imagePullPolicy: fc.imagePullPolicy,
-  };
-  if (fc.advance) {
-    // 资源设置
-    if (
-      fc.cpuRequst !== '' ||
-      fc.cpuLimit !== '' ||
-      fc.memoryRequst !== '' ||
-      fc.memoryLimit !== ''
-    ) {
-      let resources = {};
-      if (fc.cpuLimit !== '' || fc.memoryLimit !== '') {
-        resources['limits'] = {};
-        if (fc.cpuLimit !== '') {
-          resources['limits']['cpu'] = fc.cpuLimit;
+  let targetContainers = [];
+  for (let fc of data.containers) {
+    let targetContainer = {
+      name: fc.name,
+      image: fc.image,
+      imagePullPolicy: fc.imagePullPolicy,
+    };
+    if (fc.advance) {
+      // 资源设置
+      if (
+        fc.cpuRequst !== '' ||
+        fc.cpuLimit !== '' ||
+        fc.memoryRequst !== '' ||
+        fc.memoryLimit !== ''
+      ) {
+        let resources = {};
+        if (fc.cpuLimit !== '' || fc.memoryLimit !== '') {
+          resources['limits'] = {};
+          if (fc.cpuLimit !== '') {
+            resources['limits']['cpu'] = fc.cpuLimit;
+          }
+          if (fc.memoryLimit !== '') {
+            resources['limits']['memory'] = fc.memoryLimit;
+          }
         }
-        if (fc.memoryLimit !== '') {
-          resources['limits']['memory'] = fc.memoryLimit;
+
+        if (fc.cpuRequst !== '' || fc.memoryRequst !== '') {
+          resources['requests'] = {};
+          if (fc.cpuRequst !== '') {
+            resources['requests']['cpu'] = fc.cpuRequst;
+          }
+          if (fc.memoryRequst !== '') {
+            resources['requests']['memory'] = fc.memoryRequst;
+          }
         }
+        targetContainer['resources'] = resources;
       }
 
-      if (fc.cpuRequst !== '' || fc.memoryRequst !== '') {
-        resources['requests'] = {};
-        if (fc.cpuRequst !== '') {
-          resources['requests']['cpu'] = fc.cpuRequst;
-        }
-        if (fc.memoryRequst !== '') {
-          resources['requests']['memory'] = fc.memoryRequst;
-        }
+      if (fc.choicePort) {
+        targetContainer['ports'] = fc.ports;
       }
-      targetContainer['resources'] = resources;
-    }
 
-    if (fc.choicePort) {
-      targetContainer['ports'] = fc.ports;
-    }
-
-    if (fc.choiceEnv) {
-      let es = [];
-      for (let e of fc.envs) {
-        es.push({ name: e.key, value: e.value });
+      if (fc.choiceEnv) {
+        let es = [];
+        for (let e of fc.envs) {
+          es.push({ name: e.key, value: e.value });
+        }
+        targetContainer['env'] = es;
       }
-      targetContainer['env'] = es;
-    }
-    if (fc.choiceCmd) {
-      if (fc.cmds.cmd !== '') {
-        targetContainer['command'] = fc.cmds.cmd.split(',');
+      if (fc.choiceCmd) {
+        if (fc.cmds.cmd !== '') {
+          targetContainer['command'] = fc.cmds.cmd.split(',');
+        }
+        if (fc.cmds.args !== '') {
+          targetContainer['args'] = fc.cmds.args.split(',');
+        }
       }
-      if (fc.cmds.args !== '') {
-        targetContainer['args'] = fc.cmds.args.split(',');
-      }
+      targetContainers.push(targetContainer);
     }
-    targetContainers.push(targetContainer);
+    tplSpec['containers'] = targetContainers;
   }
-  tplSpec['containers'] = targetContainers;
-
   return tplSpec;
-}
+};
 export { makePodTemplate };
