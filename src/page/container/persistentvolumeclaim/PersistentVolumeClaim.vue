f355d02aa2762e246108fdd5e43e1e4d1f54bb53
caoyingjunz <cao.yingjunz@gmail.com>
2024-11-10 19:20:44 +0800

    add

--- src/page/container/persistentvolumeclaim/PersistentVolumeClaim.vue
+++ src/page/container/persistentvolumeclaim/PersistentVolumeClaim.vue
@@ -1,1 +1,10 @@
-<template>pvc</template>
+<template>
+  <Description :description="'持久卷声明定义了存储需求，Kubernetes 会根据持久卷声明创建持久卷。'" />
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+import Description from '@/components/description/index.vue';
+</script>
+
+<style></style>
