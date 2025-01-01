851a7651582c962014445b366ad5e8a5507e5544
caoyingjunz <cao.yingjunz@gmail.com>
2025-01-01 18:16:10 +0800

    add

---
+++ src/page/container/release/releaseDetail.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-card class="contend-card-container2" style="margin-top: 1px">
+    <div style="margin-top: 10px; float: right">
+      <button class="pixiu-two-button2" style="width: 60px" @click="goToNode">返回</button>
+    </div>
+
+    <el-space style="display: flex; margin: 20px 15px">
+      <div style="display: flex; align-items: center; height: 100%">
+        <pixiu-icon name="icon-zongrongqizu" size="75px" type="iconfont" color="#006eff" />
+      </div>
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          min-height: 80px;
+        "
+      >
+        <div
+          class="breadcrumb-create-style"
+          style="font-size: 17px; margin-top: -10px; margin-left: 10px"
+        >
+          {{ data.name }}
+        </div>
+
+        <div style="margin-bottom: 10px">
+          <span class="detail-card-key-style" style="font-size: 12.5px">创建时间 </span>
+          <span class="detail-card-value-style" style="margin-left: 5px; font-size: 12.5px">
+            {{ data.createTime }}</span
+          >
+        </div>
+
+        <div style="margin-bottom: -10px; margin-left: 10px">
+          <button class="pixiu-two-button" @click="GetNode">刷新</button>
+
+          <button class="pixiu-two-button2" style="margin-left: 10px" @click="handleLogDrawer">
+            日志
+          </button>
+
+          <button
+            class="pixiu-two-button2"
+            style="margin-left: 10px; width: 85px"
+            @click="viewYaml"
+          >
+            查看YAML
+          </button>
+
+          <button
+            class="pixiu-two-button2"
+            style="margin-left: 10px; width: 85px"
+            @click="handleRemoteLoginDialog"
+          >
+            远程登录
+          </button>
+
+          <button class="pixiu-two-button2" style="margin-left: 10px; width: 85px; color: #171313">
+            更多操作
+          </button>
+        </div>
+      </div>
+    </el-space>
+  </el-card>
+</template>
