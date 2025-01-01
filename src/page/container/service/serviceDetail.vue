df2be4c1e7372554a20b2d7197e41649058c46b0
caoyingjunz <cao.yingjunz@gmail.com>
2025-01-01 23:01:00 +0800

    add

--- src/page/container/service/serviceDetail.vue
+++ src/page/container/service/serviceDetail.vue
@@ -67,7 +67,9 @@
       <el-form style="margin-top: 10px">
         <el-form-item>
           <template #label>
-            <span class="detail-card-key-style" style="font-size: 14px; color: #040000"
+            <span
+              class="detail-card-key-style"
+              style="font-size: 14px; color: black; font-weight: bold"
               >实例信息
             </span>
           </template>
@@ -75,10 +77,8 @@
 
         <el-row :gutter="20">
           <el-col :span="8">
-            <span class="detail-card-key-style">名称 </span>
-            <span class="detail-card-value-style" style="margin-left: 55px">
-              {{ data.object.metadata.name }}</span
-            >
+            <span class="detail-card-key-style">实例名称 </span>
+            <span class="detail-card-value-style"> {{ data.object.metadata.name }}</span>
           </el-col>
           <el-col :span="8">
             <el-row>
@@ -159,135 +159,36 @@
         </el-form-item>
       </el-form>
     </div>
-  </el-card>
 
-  <div v-if="data.activeName === 'first'">
-    <el-card class="contend-card-container2">
-      <div class="big-world-style" style="margin-bottom: 20px; margin-top: 6px">基本信息</div>
-
-      <div v-if="data.service.metadata" style="margin-top: 8px; width: 100%; border-radius: 0px">
-        <el-form-item label="名称" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 90px">
-            {{ data.service.metadata.name }}
-          </span>
-        </el-form-item>
-        <el-form-item label="命名空间" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 63px">
-            {{ data.service.metadata.namespace }}
-          </span>
-        </el-form-item>
-        <el-form-item label="创建时间" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 63px">
-            {{ data.createTime }}
-          </span>
-        </el-form-item>
-        <el-form-item label="Labels" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 75px">
-            <div
-              v-if="
-                data.service.spec.selector === undefined ||
-                data.service.spec.selector.matchLabels === undefined
-              "
-            >
-              -
-            </div>
-            <div v-else>
-              <div
-                v-for="(item, index) in data.service.spec.selector.matchLabels"
-                :key="item"
-                style="margin-top: -1px"
-              >
-                {{ index }}: {{ item }}
-              </div>
-            </div>
-          </span>
-        </el-form-item>
-
-        <el-form-item label="Annotations" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 42px">
-            <div v-if="data.service.metadata.annotations === undefined">-</div>
-            <div v-else>
-              <div
-                v-for="(item, index) in data.service.metadata.annotations"
-                :key="item"
-                style="margin-top: -1px"
-              >
-                {{ index }}: {{ item }}
-              </div>
-            </div>
-          </span>
-        </el-form-item>
-
-        <el-form-item label="类型" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 88px">
-            {{ data.service.spec.type }}
-          </span>
-        </el-form-item>
-
-        <el-form-item label="服务IP" class="detail-tab-style">
-          <span class="detail-base-info-style" style="margin-left: 75px">
-            {{ data.service.spec.clusterIP }}
-          </span>
-        </el-form-item>
-      </div></el-card
-    >
-  </div>
-
-  <div v-if="data.activeName === 'second'">
-    <div style="margin-top: 20px">
-      <el-row>
-        <el-col>
-          <button class="pixiu-two-button">刷新</button>
-        </el-col>
-      </el-row>
-
-      <el-card class="contend-card-container2">
-        <el-table
-          v-loading="data.loading"
-          :data="data.service.spec.ports"
-          stripe
-          style="margin-top: 10px; width: 100%; margin-bottom: 25px"
-          header-row-class-name="pixiu-table-header"
-          :cell-style="{
-            'font-size': '12px',
-            color: '#191919',
-          }"
-        >
-          <el-table-column type="selection" width="30px" />
-          <el-table-column prop="name" label="端口名称" />
-          <el-table-column prop="port" label="服务端口" />
-          <el-table-column prop="protocol" label="协议"> </el-table-column>
-          <el-table-column prop="targetPort" label="目标端口"> </el-table-column>
-        </el-table>
+    <div v-if="data.activeName === 'second'">
+      <el-card class="detail-docs" style="margin-left: 10px">
+        <el-icon
+          style="vertical-align: middle; font-size: 16px; margin-left: -25px; margin-top: -50px"
+          ><WarningFilled
+        /></el-icon>
+        <div style="vertical-align: middle; margin-top: -40px">展示 service 关联的全部端口组</div>
       </el-card>
-    </div>
-  </div>
 
-  <div v-if="data.activeName === 'five'">
-    <div style="margin-top: 20px">
-      <el-col>
-        <button class="pixiu-two-button" style="width: 85px" @click="editYaml">编辑YAML</button>
-        <button class="pixiu-two-button" style="margin-left: 10px" @click="copyYmal">复制</button>
-        <div style="margin-left: 8px; float: right; margin-top: 6px">
-          <pixiu-icon
-            name="icon-icon-refresh"
-            style="cursor: pointer"
-            size="14px"
-            type="iconfont"
-            color="#909399"
-            @click="GetService"
-          />
-        </div>
-      </el-col>
+      <el-table
+        v-loading="data.loading"
+        :data="data.object.spec.ports"
+        stripe
+        style="margin-top: 2px"
+        header-row-class-name="pixiu-table-header"
+        :cell-style="{
+          'font-size': '12px',
+          color: '#191919',
+        }"
+      >
+        <el-table-column type="selection" width="30px" />
+        <el-table-column prop="name" label="端口名称" />
+        <el-table-column prop="port" label="服务端口" />
+        <el-table-column prop="protocol" label="协议"> </el-table-column>
+        <el-table-column prop="targetPort" label="目标端口"> </el-table-column>
+      </el-table>
+      <div style="margin-top: 20px"></div>
     </div>
-    <div style="margin-top: 15px"></div>
-
-    <MyCodeMirror :yaml="data.yaml" :read-only="data.readOnly" :height="420"></MyCodeMirror>
-    <div v-if="!data.readOnly" style="margin-top: 10px">
-      <el-button class="pixiu-cancel-button" @click="cancel()">取消</el-button>
-      <el-button class="pixiu-confirm-button" type="primary" @click="confirm()">确定</el-button>
-    </div>
-  </div>
+  </el-card>
 </template>
 
 <script setup lang="jsx">
@@ -365,6 +266,7 @@ const GetService = async () => {
       url: `/pixiu/proxy/${data.cluster}/api/v1/namespaces/${data.namespace}/services/${data.name}`,
     });
     data.service = res;
+    data.object = res;
 
     data.createTime = formatTimestamp(data.service.metadata.creationTimestamp);
     data.yaml = jsYaml.dump(data.service, { quotingType: '"' });
