157bb779b2b44348faa0f57f72b82109a84327ab
caoyingjunz <cao.yingjunz@gmail.com>
2025-01-01 17:46:52 +0800

    修复helm release 查询接口

--- src/services/kubernetes/releaseService.js
+++ src/services/kubernetes/releaseService.js
@@ -5,7 +5,7 @@ export const getReleaseList = async (cluster, namespace) => {
   const [err, result] = await awaitWrap(
     http({
       method: 'get',
-      url: `/pixiu/helms/clusters/${cluster}/v1/namespaces/${namespace}/releases`,
+      url: `/pixiu/helms/clusters/${cluster}/namespaces/${namespace}/releases`,
       data: {
         limit: 500,
       },
@@ -18,7 +18,17 @@ export const deleteRelease = async (cluster, namespace, name) => {
   const [err, result] = await awaitWrap(
     http({
       method: 'delete',
-      url: `/pixiu/helms/clusters/${cluster}/v1/namespaces/${namespace}/releases/${name}`,
+      url: `/pixiu/helms/clusters/${cluster}/namespaces/${namespace}/releases/${name}`,
+    }),
+  );
+  return [result, err];
+};
+
+export const getRelease = async (cluster, namespace, name) => {
+  const [err, result] = await awaitWrap(
+    http({
+      method: 'get',
+      url: `/pixiu/helms/clusters/${cluster}/namespaces/${namespace}/releases/${name}`,
     }),
   );
   return [result, err];
