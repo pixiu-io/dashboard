feb069162b326110f0309abcfb47ecca4979e1f6
caoyingjunz <cao.yingjunz@gmail.com>
2025-01-02 23:22:40 +0800

    重构 deployment 详情页面

--- src/page/container/deployment/details.vue
+++ src/page/container/deployment/details.vue
@@ -1,4 +1,62 @@
 <template>
+  <el-card class="contend-card-container2" style="margin-top: 1px">
+    <div style="margin-top: 10px; float: right">
+      <button class="pixiu-two-button2" style="width: 60px" @click="got">返回</button>
+    </div>
+
+    <el-space style="display: flex; margin: 20px 15px">
+      <div style="display: flex; align-items: center; height: 100%">
+        <pixiu-icon name="icon-zongrongqizu" size="75px" type="iconfont" color="#006eff" />
+      </div>
+      <div
+        style="
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          min-height: 80px;
+        "
+      >
+        <div
+          class="breadcrumb-create-style"
+          style="font-size: 17px; margin-top: -10px; margin-left: 10px"
+        >
+          {{ data.name }}
+        </div>
+
+        <div style="margin-bottom: 10px">
+          <span class="detail-card-key-style" style="font-size: 12.5px">创建时间 </span>
+          <span class="detail-card-value-style" style="margin-left: 5px; font-size: 12.5px">
+            {{ data.createTime }}</span
+          >
+        </div>
+
+        <div style="margin-bottom: -10px; margin-left: 10px">
+          <button class="pixiu-two-button" @click="GetNode">刷新</button>
+
+          <button
+            class="pixiu-two-button2"
+            style="margin-left: 10px; width: 85px"
+            @click="viewYaml"
+          >
+            查看YAML
+          </button>
+
+          <button
+            class="pixiu-two-button2"
+            style="margin-left: 10px; width: 85px"
+            @click="handleRemoteLoginDialog"
+          >
+            远程登录
+          </button>
+
+          <button class="pixiu-two-button2" style="margin-left: 10px; width: 85px; color: #171313">
+            更多操作
+          </button>
+        </div>
+      </div>
+    </el-space>
+  </el-card>
+
   <el-card class="detail-card-container">
     <div style="margin-top: 10px; float: right">
       <button class="pixiu-two-button2" style="width: 60px; margin-right: 6px">日志</button>
@@ -1031,28 +1089,4 @@ const editYaml = () => {
   data.readOnly = false;
 };
 </script>
-
-<style scoped="scoped">
-.deployment-tab {
-  margin-top: 1px;
-  margin-bottom: -32px;
-}
-
-.demo-tabs .el-tabs__content {
-  padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
-}
-
-.deployment-info {
-  color: #909399;
-  font-size: 13px;
-  margin-left: 8px;
-}
-
-.deploy-detail-info {
-  font-size: 13px;
-  color: #29232b;
-}
-</style>
+<style lang="scss" scoped></style>
